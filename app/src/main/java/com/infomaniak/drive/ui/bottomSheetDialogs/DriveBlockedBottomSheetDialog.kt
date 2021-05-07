f85b66e33e089f5f5547f8b9018165ba05179327
Kilian Périsset <kilian.perisset@infomaniak.com>
2021-05-07 16:09:47 +0200

    Implemented orderDrive URL opening in MaintenanceActivity
    🔥
    
    Signed-off-by: Kilian Périsset <kilian.perisset@infomaniak.com>

--- app/src/main/java/com/infomaniak/drive/ui/bottomSheetDialogs/DriveBlockedBottomSheetDialog.kt
+++ app/src/main/java/com/infomaniak/drive/ui/bottomSheetDialogs/DriveBlockedBottomSheetDialog.kt
@@ -20,7 +20,7 @@ package com.infomaniak.drive.ui.bottomSheetDialogs
 import android.os.Bundle
 import androidx.navigation.fragment.navArgs
 import com.infomaniak.drive.R
-import com.infomaniak.drive.data.api.ApiRoutes.orderDrive
+import com.infomaniak.drive.data.api.ApiRoutes
 import com.infomaniak.lib.core.utils.UtilsUi.openUrl
 import com.infomaniak.lib.core.utils.toPx
 import kotlinx.android.synthetic.main.fragment_bottom_sheet_information.*
@@ -43,7 +43,7 @@ class DriveBlockedBottomSheetDialog : InformationBottomSheetDialog() {
         actionButton.apply {
             setText(R.string.buttonRenew)
             setOnClickListener {
-                requireContext().openUrl(orderDrive()) // Open renew URL from product, TODO : Awaiting invoices scope
+                requireContext().openUrl(ApiRoutes.orderDrive()) // Open renew URL from product, TODO : Awaiting invoices scope
             }
         }
         secondaryActionButton.setText(R.string.buttonClose)
