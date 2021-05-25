fb6451574bb5760225d3fde350605f213f5c8eb9
Abdourahamane BOINAIDI <abdourahamane.boinaidi@infomaniak.com>
2021-05-25 15:18:41 +0200

    on move a file from favorite view, keep file
    Signed-off-by: Abdourahamane BOINAIDI <abdourahamane.boinaidi@infomaniak.com>

--- app/src/main/java/com/infomaniak/drive/ui/fileList/FavoritesFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/fileList/FavoritesFragment.kt
@@ -21,6 +21,7 @@ import android.os.Bundle
 import androidx.navigation.fragment.findNavController
 import com.infomaniak.drive.R
 import com.infomaniak.drive.utils.Utils
+import com.infomaniak.drive.utils.Utils.OTHER_ROOT_ID
 import com.infomaniak.drive.utils.Utils.ROOT_ID
 import com.infomaniak.drive.utils.safeNavigate
 import kotlinx.android.synthetic.main.fragment_file_list.*
@@ -30,7 +31,8 @@ class FavoritesFragment : FileListFragment() {
     override var enabledMultiSelectMode: Boolean = false
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        if (folderID == ROOT_ID) downloadFiles = DownloadFiles()
+        if (folderID == ROOT_ID) folderID = OTHER_ROOT_ID
+        if (folderID == OTHER_ROOT_ID) downloadFiles = DownloadFiles()
         super.onActivityCreated(savedInstanceState)
 
         noFilesLayout.setup(
