774775d632650f30048e3907d2ca1ec84b6c7b29
Abdourahamane BOINAIDI <abdourahamane.boinaidi@infomaniak.com>
2021-07-01 18:58:47 +0200

    fix offline notification
    Signed-off-by: Abdourahamane BOINAIDI <abdourahamane.boinaidi@infomaniak.com>

--- app/src/main/java/com/infomaniak/drive/data/services/DownloadWorker.kt
+++ app/src/main/java/com/infomaniak/drive/data/services/DownloadWorker.kt
@@ -110,7 +110,7 @@ class DownloadWorker(private val context: Context, workerParams: WorkerParameter
                 downloadNotification.apply {
                     setContentText("$progress%")
                     setProgress(100, progress, false)
-                    launch { setForeground(ForegroundInfo(file.id, build())) }
+                    runBlocking { setForeground(ForegroundInfo(file.id, build())) }
                 }
             }
 
