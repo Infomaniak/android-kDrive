f1cc0b06d10b854407107bf733163e0794c8d5c8
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2021-11-19 10:19:20 +0100

    Display categories in file list view
    Signed-off-by: Kevin Boulongne <kevin.boulongne@infomaniak.com>

---
+++ app/src/main/java/com/infomaniak/drive/views/CategoryIconView.kt
@@ -0,0 +1,67 @@
+/*
+ * Infomaniak kDrive - Android
+ * Copyright (C) 2021 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.drive.views
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import com.infomaniak.drive.R
+import com.infomaniak.drive.data.models.drive.Category
+import com.infomaniak.drive.utils.isPositive
+import kotlinx.android.synthetic.main.item_user_avatar.view.*
+
+class CategoryIconView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    init {
+        inflate(context, R.layout.item_category_icon, this)
+    }
+
+    fun setCategoryIconOrHide(category: Category?) {
+        category?.let {
+            isVisible = true
+            remainingText.isGone = true
+            avatarImageView.setBackgroundColor(Color.parseColor(category.color))
+        } ?: run {
+            isGone = true
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun setRemainingCategoriesNumber(number: Int, category: Category?) {
+        if (category != null) {
+            avatarImageView.setBackgroundColor(Color.parseColor(category.color))
+            if (number.isPositive()) {
+                remainingText.text = "+$number"
+                remainingText.isVisible = true
+            } else {
+                remainingText.isGone = true
+            }
+            isVisible = true
+        } else {
+            isGone = true
+        }
+    }
+}
