6b8a01738e49d5bbf46f3f13cc591b1ffa2d6995
Abdourahamane BOINAIDI <abdourahamane.boinaidi@infomaniak.com>
2021-12-09 15:37:37 +0100

    temp fix IndexOutOfBoundsException with more sentry logs
    Signed-off-by: Abdourahamane BOINAIDI <abdourahamane.boinaidi@infomaniak.com>

---
+++ app/src/main/java/com/infomaniak/drive/utils/CustomGridLayoutManager.kt
@@ -0,0 +1,36 @@
+/*
+ * Infomaniak kDrive - Android
+ * Copyright (C) 2021 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.drive.utils
+
+import android.content.Context
+import androidx.navigation.NavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.infomaniak.drive.utils.CustomLinearLayoutManager.Companion.layoutManagerSentryLog
+
+class CustomGridLayoutManager(private val navController: NavController, context: Context?, spanCount: Int) :
+    GridLayoutManager(context, spanCount) {
+
+    override fun onLayoutChildren(recycler: RecyclerView.Recycler?, state: RecyclerView.State?) {
+        try {
+            super.onLayoutChildren(recycler, state)
+        } catch (exception: IndexOutOfBoundsException) {
+            layoutManagerSentryLog(navController, exception)
+        }
+    }
+}
