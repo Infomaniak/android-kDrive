c6eb25e1743405c646cdd2a7e93e9f3640cafbe8
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2021-12-15 12:10:32 +0100

    Replaced FlexLayout with RecyclerView + GridLayoutManager
    Signed-off-by: Kevin Boulongne <kevin.boulongne@infomaniak.com>

--- app/src/main/java/com/infomaniak/drive/ui/bottomSheetDialogs/CreateOrEditCategoryBottomSheetDialog.kt
+++ app/src/main/java/com/infomaniak/drive/ui/bottomSheetDialogs/CreateOrEditCategoryBottomSheetDialog.kt
@@ -17,14 +17,11 @@
  */
 package com.infomaniak.drive.ui.bottomSheetDialogs
 
-import android.annotation.SuppressLint
-import android.content.res.ColorStateList
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.toColorInt
 import androidx.core.os.bundleOf
 import androidx.core.view.isGone
 import androidx.core.widget.addTextChangedListener
@@ -34,31 +31,31 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.google.android.material.button.MaterialButton
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.infomaniak.drive.R
 import com.infomaniak.drive.data.api.ApiRepository
 import com.infomaniak.drive.data.api.ErrorCode.Companion.translateError
 import com.infomaniak.drive.data.cache.DriveInfosController
 import com.infomaniak.drive.data.models.drive.Category
 import com.infomaniak.drive.utils.Utils
+import com.infomaniak.drive.utils.getScreenSizeInDp
 import com.infomaniak.drive.utils.setBackNavigationResult
-import com.infomaniak.drive.utils.setMargin
 import com.infomaniak.drive.views.FullScreenBottomSheetDialog
 import com.infomaniak.lib.core.models.ApiResponse
 import com.infomaniak.lib.core.utils.hideProgress
 import com.infomaniak.lib.core.utils.showProgress
-import com.infomaniak.lib.core.utils.toPx
+import com.infomaniak.lib.core.utils.toDp
 import kotlinx.android.synthetic.main.fragment_create_category.*
 import kotlinx.coroutines.Dispatchers
+import kotlin.math.max
 
 class CreateOrEditCategoryBottomSheetDialog : FullScreenBottomSheetDialog() {
 
     private val navigationArgs: CreateOrEditCategoryBottomSheetDialogArgs by navArgs()
     private val createOrEditCategoryViewModel: CreateOrEditCategoryViewModel by viewModels()
     private val selectCategoriesViewModel: SelectCategoriesViewModel by viewModels()
-
-    private val colorLayouts = mutableListOf<MaterialButton>()
-    private var colorSelectedPosition: Int = -1
+    private val colorsAdapter: CreateOrEditCategoryAdapter by lazy { CreateOrEditCategoryAdapter() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.fragment_create_category, container, false)
@@ -66,40 +63,63 @@ class CreateOrEditCategoryBottomSheetDialog : FullScreenBottomSheetDialog() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
-
-        categoryNameValueInput.addTextChangedListener { saveButton.isEnabled = it.toString().isNotEmpty() }
-
+        configureAdapter()
         configureUI()
-        configureSaveButton()
-        configureColorsGrid()
+    }
 
-        val colorPosition = CATEGORY_COLORS.indexOfFirst { it == navigationArgs.categoryColor }.let { if (it == -1) 0 else it }
-        onColorClicked(colorPosition)
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        configCategoriesLayoutManager()
+    }
+
+    private fun configureAdapter() {
+        colorsAdapter.apply {
+            selectedPosition = CATEGORY_COLORS.indexOfFirst { it == navigationArgs.categoryColor }.let { if (it == -1) 0 else it }
+            configCategoriesLayoutManager()
+            categoriesRecyclerView.adapter = this
+            stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+            colors = ArrayList(CATEGORY_COLORS)
+        }
+    }
+
+    private fun configCategoriesLayoutManager() {
+        val numCategoriesColumns = getNumColorsColumns()
+        val gridLayoutManager = GridLayoutManager(context, numCategoriesColumns)
+        categoriesRecyclerView.layoutManager = gridLayoutManager
+    }
+
+    private fun getNumColorsColumns(minColumns: Int = 1, expectedItemSize: Int = 52): Int {
+        val screenWidth = requireActivity().getScreenSizeInDp().x
+        val margins = resources.getDimensionPixelSize(R.dimen.marginStandardSmall).toDp() * 2
+        val layoutWidth = screenWidth - margins
+        return max(minColumns, layoutWidth / expectedItemSize)
     }
 
     private fun configureUI() = with(navigationArgs) {
+        toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+
         val isCreateCategory = categoryId == CREATE_CATEGORY_ID
 
         appBarTitle.title = getString(if (isCreateCategory) R.string.createCategoryTitle else R.string.editCategoryTitle)
         editCategoryWarning.isGone = isCreateCategory
-        categoryNameValueInput.setText(categoryName)
+        categoryNameValueInput.apply {
+            addTextChangedListener { saveButton.isEnabled = it.toString().isNotEmpty() }
+            setText(categoryName)
+        }
         categoryNameValueLayout.isGone = categoryIsPredefined
 
-        saveButton.isEnabled = categoryIsPredefined || !isCreateCategory || categoryName.isNotEmpty()
-    }
-
-    private fun configureSaveButton() = with(navigationArgs) {
-        saveButton.setOnClickListener {
-            saveButton.showProgress()
-            if (categoryId == CREATE_CATEGORY_ID) createCategory() else editCategory(categoryId)
+        saveButton.apply {
+            isEnabled = categoryIsPredefined || !isCreateCategory || categoryName.isNotEmpty()
+            setOnClickListener {
+                showProgress()
+                if (categoryId == CREATE_CATEGORY_ID) createCategory() else editCategory(categoryId)
+            }
         }
     }
 
     private fun createCategory() {
         val name = categoryNameValueInput.text.toString()
-        val color = CATEGORY_COLORS[colorSelectedPosition]
+        val color = CATEGORY_COLORS[colorsAdapter.selectedPosition]
 
         createOrEditCategoryViewModel.createCategory(navigationArgs.driveId, name, color)
             .observe(viewLifecycleOwner) { createResponse ->
@@ -133,7 +153,7 @@ class CreateOrEditCategoryBottomSheetDialog : FullScreenBottomSheetDialog() {
 
     private fun editCategory(categoryId: Int) {
         val name = categoryNameValueInput.text.toString().let { if (it.isEmpty()) null else it }
-        val color = CATEGORY_COLORS[colorSelectedPosition]
+        val color = CATEGORY_COLORS[colorsAdapter.selectedPosition]
         val driveId = navigationArgs.driveId
 
         createOrEditCategoryViewModel.editCategory(driveId, categoryId, name, color).observe(viewLifecycleOwner) { apiResponse ->
@@ -149,45 +169,6 @@ class CreateOrEditCategoryBottomSheetDialog : FullScreenBottomSheetDialog() {
                 saveButton.hideProgress(R.string.buttonSave)
                 Utils.showSnackbar(requireView(), apiResponse.translateError())
             }
-        }
-    }
-
-    @SuppressLint("InflateParams")
-    private fun configureColorsGrid() {
-        CATEGORY_COLORS.forEach { color ->
-
-            // Create view
-            (layoutInflater.inflate(R.layout.view_category_color, null) as MaterialButton).apply {
-
-                // Set layout size
-                val size = 36.toPx()
-                layoutParams = ViewGroup.MarginLayoutParams(size, size)
-
-                // Set layout margins
-                val margin = resources.getDimensionPixelSize(R.dimen.marginStandardSmall)
-                setMargin(margin, margin, margin, margin)
-
-                // Set background & icon colors
-                val colorStateList = ColorStateList.valueOf(color.toColorInt())
-                backgroundTintList = colorStateList
-                iconTint = colorStateList
-
-                // Set click
-                val position = colorLayouts.size
-                setOnClickListener { onColorClicked(position) }
-
-                // Add view
-                colorLayouts.add(this)
-                categoriesFlexBoxLayout.addView(this)
-            }
-        }
-    }
-
-    private fun onColorClicked(position: Int) {
-        if (position != colorSelectedPosition) {
-            colorLayouts.getOrNull(colorSelectedPosition)?.let { it.iconTint = it.backgroundTintList }
-            colorLayouts[position].iconTint = ContextCompat.getColorStateList(requireContext(), R.color.white)
-            colorSelectedPosition = position
         }
     }
 
