e6d419acaab211c4fdfb32d85416552b9352d739
Abdourahamane BOINAIDI <abdourahamane.boinaidi@infomaniak.com>
2022-01-14 17:38:01 +0100

    set min date to reload cache to 3 month
    Signed-off-by: Abdourahamane BOINAIDI <abdourahamane.boinaidi@infomaniak.com>

--- app/src/main/java/com/infomaniak/drive/data/cache/FileController.kt
+++ app/src/main/java/com/infomaniak/drive/data/cache/FileController.kt
@@ -57,7 +57,7 @@ object FileController {
     private val PICTURES_FILE = File(PICTURES_FILE_ID, name = "Pictures")
     private val RECENT_CHANGES_FILE = File(RECENT_CHANGES_FILE_ID, name = "Recent changes")
 
-    private val minDateToIgnoreCache = Calendar.getInstance().apply { add(Calendar.MONTH, -5) }.get(Calendar.MONTH) // 6 month
+    private val minDateToIgnoreCache = Calendar.getInstance().apply { add(Calendar.MONTH, -2) }.get(Calendar.MONTH) // 3 month
 
     private fun getFileById(realm: Realm, fileId: Int) = realm.where(File::class.java).equalTo("id", fileId).findFirst()
 
