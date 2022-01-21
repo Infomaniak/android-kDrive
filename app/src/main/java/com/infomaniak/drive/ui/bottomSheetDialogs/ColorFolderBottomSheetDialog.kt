7129fcef41d1a225d51435aa75ee7452e4a6a892
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2022-01-21 13:45:57 +0100

    Fixed ColorFolder GridLayout manager
    Signed-off-by: Kevin Boulongne <kevin.boulongne@infomaniak.com>

--- app/src/main/java/com/infomaniak/drive/ui/bottomSheetDialogs/ColorFolderBottomSheetDialog.kt
+++ app/src/main/java/com/infomaniak/drive/ui/bottomSheetDialogs/ColorFolderBottomSheetDialog.kt
@@ -23,17 +23,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.infomaniak.drive.R
 import com.infomaniak.drive.ui.bottomSheetDialogs.ColorFolderAdapter.Companion.COLORS
-import com.infomaniak.drive.utils.getScreenSizeInDp
 import com.infomaniak.drive.utils.setBackNavigationResult
-import com.infomaniak.lib.core.utils.toDp
 import kotlinx.android.synthetic.main.fragment_bottom_sheet_color_folder.*
-import kotlin.math.max
-
 
 open class ColorFolderBottomSheetDialog : BottomSheetDialogFragment() {
 
@@ -46,35 +41,27 @@ open class ColorFolderBottomSheetDialog : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        configureColorsAdapter()
+        setAdapter()
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        configCategoriesLayoutManager()
+        setLayoutManager()
     }
 
-    private fun configureColorsAdapter() {
+    private fun setAdapter() {
         colorsAdapter = ColorFolderAdapter(
             onColorSelected = { color: String -> setBackNavigationResult(COLOR_FOLDER_NAV_KEY, color) }
         ).apply {
             selectedPosition = COLORS.indexOfFirst { it == navigationArgs.color }
-            configCategoriesLayoutManager()
+            setLayoutManager()
             colorsRecyclerView.adapter = this
             stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
         }
     }
 
-    private fun configCategoriesLayoutManager() {
-        val numCategoriesColumns = getNumColorsColumns()
-        val gridLayoutManager = GridLayoutManager(context, numCategoriesColumns)
-        colorsRecyclerView.layoutManager = gridLayoutManager
-    }
-
-    private fun getNumColorsColumns(minColumns: Int = 1, expectedItemSize: Int = 56): Int {
-        val screenWidth = requireActivity().getScreenSizeInDp().x
-        val margins = resources.getDimensionPixelSize(R.dimen.marginStandardSmall).toDp() * 2
-        return max(minColumns, (screenWidth - margins) / expectedItemSize)
+    private fun setLayoutManager() {
+        colorsRecyclerView.layoutManager = AutoSpanGridLayoutManager(requireContext(), 0)
     }
 
     companion object {
