7e28e6f19de5e1323b0f207e5568cbea7633a5af
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2022-01-26 12:13:03 +0100

    Updated copyright to 2022
    Signed-off-by: Kevin Boulongne <kevin.boulongne@infomaniak.com>

--- app/src/main/java/com/infomaniak/drive/views/com/liuzhenlin/simrv/SlidingItemMenuRecyclerView.kt
+++ app/src/main/java/com/infomaniak/drive/views/com/liuzhenlin/simrv/SlidingItemMenuRecyclerView.kt
@@ -1,7 +1,7 @@
 /*
  * Infomaniak kDrive - Android
  * Copyright (C) 2017–2020 刘振林 "lzls". All rights reserved. File initially licensed under Apache license 2.0 : http://www.apache.org/licenses/LICENSE-2.0
- * Copyright (C) 2021 Infomaniak Network SA
+ * Copyright (C) 2022 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
