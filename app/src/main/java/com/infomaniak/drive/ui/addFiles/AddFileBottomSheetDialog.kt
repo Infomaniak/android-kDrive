bdd0978a8f739165c3acfe4df997bc4a914e570c
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2022-02-07 13:40:17 +0100

    Removed StartActivityForResult in AddFileBottomSheetDialog
    Signed-off-by: Kevin Boulongne <kevin.boulongne@infomaniak.com>

--- app/src/main/java/com/infomaniak/drive/ui/addFiles/AddFileBottomSheetDialog.kt
+++ app/src/main/java/com/infomaniak/drive/ui/addFiles/AddFileBottomSheetDialog.kt
@@ -26,6 +26,7 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.core.content.FileProvider
 import androidx.documentfile.provider.DocumentFile
 import androidx.fragment.app.activityViewModels
@@ -62,11 +63,6 @@ class AddFileBottomSheetDialog : BottomSheetDialogFragment() {
     private var currentPhotoUri: Uri? = null
     private var mediaPhotoPath = ""
     private var mediaVideoPath = ""
-
-    companion object {
-        const val SELECT_FILES_REQ = 2
-        const val CAPTURE_MEDIA_REQ = 3
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return (mainViewModel.currentFolderOpenAddFileBottom.value ?: mainViewModel.currentFolder.value)?.let {
@@ -106,18 +102,6 @@ class AddFileBottomSheetDialog : BottomSheetDialogFragment() {
         mainViewModel.currentFolderOpenAddFileBottom.value = null
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (resultCode == Activity.RESULT_OK) {
-            when (requestCode) {
-                SELECT_FILES_REQ -> onSelectFilesResult(data)
-                CAPTURE_MEDIA_REQ -> onCaptureMediaResult(data)
-            }
-        }
-        dismiss()
-    }
-
     private fun openCamera() {
         if (openCameraPermissions.checkSyncPermissions()) {
             openCamera.isEnabled = false
@@ -129,13 +113,21 @@ class AddFileBottomSheetDialog : BottomSheetDialogFragment() {
                 val takeVideoIntent = Intent(MediaStore.ACTION_VIDEO_CAPTURE).apply {
                     putExtra(MediaStore.EXTRA_OUTPUT, createMediaFile(true))
                 }
-                val chooserIntent = Intent.createChooser(takePictureIntent, getString(R.string.buttonTakePhotoOrVideo))
-                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(takeVideoIntent))
-                startActivityForResult(chooserIntent, CAPTURE_MEDIA_REQ)
+                val chooserIntent = Intent.createChooser(takePictureIntent, getString(R.string.buttonTakePhotoOrVideo)).apply {
+                    putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(takeVideoIntent))
+                }
+                captureMediaResultLauncher.launch(chooserIntent)
             } catch (e: IOException) {
                 e.printStackTrace()
             }
         }
+    }
+
+    private val captureMediaResultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+        with(result) {
+            if (resultCode == Activity.RESULT_OK) onCaptureMediaResult(data)
+        }
+        dismiss()
     }
 
     private fun uploadFiles() {
@@ -148,8 +140,16 @@ class AddFileBottomSheetDialog : BottomSheetDialogFragment() {
                 putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
                 addCategory(Intent.CATEGORY_OPENABLE)
             }
-            startActivityForResult(Intent.createChooser(intent, getString(R.string.addFileSelectUploadFile)), SELECT_FILES_REQ)
+            val chooserIntent = Intent.createChooser(intent, getString(R.string.addFileSelectUploadFile))
+            selectFilesResultLauncher.launch(chooserIntent)
         }
+    }
+
+    private val selectFilesResultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+        with(result) {
+            if (resultCode == Activity.RESULT_OK) onSelectFilesResult(data)
+        }
+        dismiss()
     }
 
     private fun scanDocuments() {
