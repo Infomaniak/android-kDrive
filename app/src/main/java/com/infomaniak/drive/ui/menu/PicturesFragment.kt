74ac63b3dfd1bbba0383b13a8655100a202e6cab
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2022-03-10 15:44:47 +0100

    Boolean doesn't need to be in global scope

--- app/src/main/java/com/infomaniak/drive/ui/menu/PicturesFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/menu/PicturesFragment.kt
@@ -57,7 +57,6 @@ class PicturesFragment(private val onFinish: (() -> Unit)? = null) : MultiSelect
     private lateinit var picturesAdapter: PicturesAdapter
 
     var menuPicturesBinding: FragmentMenuPicturesBinding? = null
-    var currentlyInGallery = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_pictures, container, false)
@@ -69,7 +68,7 @@ class PicturesFragment(private val onFinish: (() -> Unit)? = null) : MultiSelect
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        currentlyInGallery = menuPicturesBinding != null
+        val currentlyInGallery = menuPicturesBinding != null
 
         noPicturesLayout.setup(
             icon = R.drawable.ic_images,
