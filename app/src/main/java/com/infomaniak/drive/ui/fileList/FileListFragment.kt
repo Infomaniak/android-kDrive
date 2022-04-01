c3d326b290629178e7cfa75ad0662795c8de3aac
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2022-04-01 15:34:52 +0200

    Cleaned code

--- app/src/main/java/com/infomaniak/drive/ui/fileList/FileListFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/fileList/FileListFragment.kt
@@ -321,7 +321,7 @@ open class FileListFragment : MultiSelectFragment(MATOMO_CATEGORY), SwipeRefresh
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         fileListViewModel.isListMode.value?.let { isListMode ->
-            if(!isListMode) binding.fileRecyclerView.layoutManager = createLayoutManager(isListMode)
+            if (!isListMode) binding.fileRecyclerView.layoutManager = createLayoutManager(isListMode)
         }
     }
 
