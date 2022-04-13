c28d5f8c28d4866fb1248d417c3f2f0d69ef8f1d
Joris Bodin <joris.bodin@infomaniak.com>
2022-04-13 12:56:44 +0200

    Extract Window.lightNavigationBar in Core and change end point

--- app/src/main/java/com/infomaniak/drive/ui/fileList/preview/PreviewSliderFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/fileList/preview/PreviewSliderFragment.kt
@@ -51,6 +51,8 @@ import com.infomaniak.drive.utils.Utils.openWith
 import com.infomaniak.drive.utils.Utils.openWithIntent
 import com.infomaniak.drive.views.FileInfoActionsView
 import com.infomaniak.lib.core.models.ApiResponse
+import com.infomaniak.lib.core.utils.lightNavigationBar
+import com.infomaniak.lib.core.utils.lightStatusBar
 import com.infomaniak.lib.core.utils.toggleEdgeToEdge
 import kotlinx.android.synthetic.main.fragment_preview_slider.*
 import kotlinx.android.synthetic.main.view_file_info_actions.view.*
