53d9f613cd5dcc38b0b51c2425201bdeec128a45
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2022-05-17 18:50:27 +0200

    Migrate realm modules with api v2

--- app/src/main/java/com/infomaniak/drive/utils/RealmModules.kt
+++ app/src/main/java/com/infomaniak/drive/utils/RealmModules.kt
@@ -19,11 +19,23 @@ package com.infomaniak.drive.utils
 
 import com.infomaniak.drive.data.models.*
 import com.infomaniak.drive.data.models.drive.*
+import com.infomaniak.drive.data.models.file.FileConversion
+import com.infomaniak.drive.data.models.file.FileVersion
+import com.infomaniak.drive.data.models.file.dropbox.DropBoxCapabilities
+import com.infomaniak.drive.data.models.file.dropbox.DropBoxSize
+import com.infomaniak.drive.data.models.file.dropbox.DropBoxValidity
+import com.infomaniak.drive.data.models.file.sharelink.ShareLinkCapabilities
 import io.realm.annotations.RealmModule
 
 object RealmModules {
 
-    @RealmModule(classes = [File::class, Rights::class, FileActivity::class, FileCategory::class])
+    @RealmModule(
+        classes = [
+            File::class, Rights::class, FileActivity::class, FileCategory::class,
+            ShareLink::class, DropBox::class, FileVersion::class, FileConversion::class,
+            DropBoxCapabilities::class, ShareLinkCapabilities::class, DropBoxValidity::class, DropBoxSize::class
+        ]
+    )
     class LocalFilesModule
 
     @RealmModule(classes = [UploadFile::class, SyncSettings::class, MediaFolder::class])
