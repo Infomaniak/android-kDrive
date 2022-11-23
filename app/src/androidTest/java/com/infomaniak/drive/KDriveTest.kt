e2c2a8c6bc00731318cb0c5c146c776e664cf7b2
Fabian Devel <fabian.devel@infomaniak.com>
2022-11-23 14:50:15 +0100

    fix permission granting for all version

--- app/src/androidTest/java/com/infomaniak/drive/KDriveTest.kt
+++ app/src/androidTest/java/com/infomaniak/drive/KDriveTest.kt
@@ -18,19 +18,15 @@
 package com.infomaniak.drive
 
 import android.content.Context
-import android.os.Build
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.runner.permission.PermissionRequester
 import com.infomaniak.drive.data.api.ApiRepository
 import com.infomaniak.drive.data.cache.FileController
 import com.infomaniak.drive.data.models.UserDrive
-import com.infomaniak.drive.utils.AccountUtils
+import com.infomaniak.drive.utils.*
 import com.infomaniak.drive.utils.AccountUtils.addUser
 import com.infomaniak.drive.utils.AccountUtils.getUserById
 import com.infomaniak.drive.utils.ApiTestUtils.assertApiResponseData
-import com.infomaniak.drive.utils.Env
-import com.infomaniak.drive.utils.KDriveHttpClient
-import com.infomaniak.drive.utils.RealmModules
 import com.infomaniak.lib.core.InfomaniakCore
 import com.infomaniak.lib.core.models.user.User
 import com.infomaniak.lib.core.networking.HttpClient
@@ -84,9 +80,7 @@ open class KDriveTest {
 
             setUpRealm()
 
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-                grantPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            }
+            grantPermissions()
         }
 
         @AfterAll
@@ -123,9 +117,9 @@ open class KDriveTest {
             }
         }
 
-        private fun grantPermissions(vararg permissions: String) {
+        private fun grantPermissions() {
             PermissionRequester().apply {
-                addPermissions(*permissions)
+                addPermissions(*DrivePermissions.permissions)
                 requestPermissions()
             }
         }
