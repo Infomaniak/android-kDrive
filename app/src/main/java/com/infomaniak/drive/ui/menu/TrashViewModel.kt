35c3c99d70c49949adf2aebdee33ef69c91a8c21
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2023-11-03 16:42:27 +0100

    feat: (ApiV3) - Use has_more in TrashViewModel

--- app/src/main/java/com/infomaniak/drive/ui/menu/TrashViewModel.kt
+++ app/src/main/java/com/infomaniak/drive/ui/menu/TrashViewModel.kt
@@ -95,17 +95,7 @@ class TrashViewModel : ViewModel() {
             val apiResponseData = apiResponse.data
             when {
                 apiResponseData.isNullOrEmpty() -> driveTrashResults.postValue(null)
-                apiResponse.cursor == null ->
-                    driveTrashResults.postValue(
-                        FolderFilesResult(
-                            files = apiResponseData,
-                            isComplete = true,
-                            isFirstPage = isFirstPage,
-                            isNewSort = isNewSort,
-                        )
-                    )
-
-                else -> {
+                apiResponse.hasMore -> {
                     driveTrashResults.postValue(
                         FolderFilesResult(
                             files = apiResponseData,
@@ -114,7 +104,17 @@ class TrashViewModel : ViewModel() {
                             isNewSort = isNewSort,
                         )
                     )
-                    recursive(isFirstPage = false, isNewSort = false, cursor = apiResponse.cursor)
+                    recursive(isFirstPage = false, isNewSort = false, cursor = apiResponse.cursor!!)
+                }
+                else -> {
+                    driveTrashResults.postValue(
+                        FolderFilesResult(
+                            files = apiResponseData,
+                            isComplete = true,
+                            isFirstPage = isFirstPage,
+                            isNewSort = isNewSort,
+                        )
+                    )
                 }
             }
         }
