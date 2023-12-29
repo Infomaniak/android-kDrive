036158b52b1a541363fd73682a73ddee420f53c9
Vincent TE <vincent.te@infomaniak.com>
2023-12-29 10:45:48 +0100

    Remove logs

--- app/src/main/java/com/infomaniak/drive/utils/FileItemUtils.kt
+++ app/src/main/java/com/infomaniak/drive/utils/FileItemUtils.kt
@@ -27,7 +27,6 @@ import android.media.ThumbnailUtils
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
-import android.util.Log
 import android.util.Size
 import android.widget.ImageView
 import android.widget.TextView
@@ -336,11 +335,8 @@ fun ProgressLayoutView.setupFileProgress(file: File, containsProgress: Boolean =
         file.isOfflineFile(context, checkLocalFile = false) -> {
             hideProgress()
             isVisible = true
-            Log.i("setupFileProgress", "setupFileProgress -> offline = true")
-            Log.i("setupFileProgress", "setupFileProgress -> ${file.isOffline}")
         }
         else -> {
-            Log.i("setupFileProgress", "setupFileProgress -> hiding progress")
             isGone = true
         }
     }
