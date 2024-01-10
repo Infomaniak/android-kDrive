6e6d7b76dc5eb54e280eed0d5b7eba4550a1cfa0
Joris Bodin <joris.bodin@infomaniak.com>
2024-01-10 18:22:00 +0100

    Unlimited token
    Expires after 6 months of non-use

--- app/src/main/java/com/infomaniak/drive/utils/Extensions.kt
+++ app/src/main/java/com/infomaniak/drive/utils/Extensions.kt
@@ -374,7 +374,12 @@ fun Activity.getAdjustedColumnNumber(expectedItemSize: Int, minColumns: Int = 2,
 fun <T> ApiResponse<ArrayList<T>>.isLastPage() = (data?.size ?: 0) < itemsPerPage
 
 fun Context.getInfomaniakLogin(): InfomaniakLogin {
-    return InfomaniakLogin(this, appUID = BuildConfig.APPLICATION_ID, clientID = BuildConfig.CLIENT_ID)
+    return InfomaniakLogin(
+        this,
+        appUID = BuildConfig.APPLICATION_ID,
+        clientID = BuildConfig.CLIENT_ID,
+        accessType = null
+    )
 }
 
 //region Worker
