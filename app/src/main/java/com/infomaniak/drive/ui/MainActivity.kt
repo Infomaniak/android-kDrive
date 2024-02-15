c732967d8080d590b76b99e7eb1a34ebb399377d
Fabian Devel <fabian.devel@infomaniak.com>
2024-02-15 16:19:20 +0100

    chores(Core): Update InAppManager

--- app/src/main/java/com/infomaniak/drive/ui/MainActivity.kt
+++ app/src/main/java/com/infomaniak/drive/ui/MainActivity.kt
@@ -143,7 +143,7 @@ class MainActivity : BaseActivity() {
             }
         }
 
-    private val inAppUpdateManager by lazy { initAppUpdateManager() }
+    private val inAppUpdateManager by lazy { InAppUpdateManager(this, BuildConfig.APPLICATION_ID, BuildConfig.VERSION_CODE) }
     private var inAppUpdateSnackbar: Snackbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -166,7 +166,7 @@ class MainActivity : BaseActivity() {
 
         LocalBroadcastManager.getInstance(this).registerReceiver(downloadReceiver, IntentFilter(DownloadReceiver.TAG))
 
-        setInAppManagerCallbacks()
+        initAppUpdateManager()
     }
 
     private fun getNavHostFragment() = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
@@ -240,36 +240,30 @@ class MainActivity : BaseActivity() {
 
 
     //region In-App Updates
-    private fun initAppUpdateManager() = InAppUpdateManager(
-        activity = this,
-        appId = BuildConfig.APPLICATION_ID,
-        versionCode = BuildConfig.VERSION_CODE,
-        onUserChoice = { isWantingUpdate -> trackInAppUpdate(if (isWantingUpdate) "discoverNow" else "discoverLater") },
-        onInstallStart = { trackInAppUpdate("installUpdate") },
-        onInstallFailure = {
-            Sentry.captureException(it)
-            showSnackbar(title = R.string.errorUpdateInstall, anchor = getMainFab())
-        },
-    )
-
-    private fun setInAppManagerCallbacks() {
-        inAppUpdateManager.apply {
+    private fun initAppUpdateManager() {
+        inAppUpdateManager.init(
+            onUserChoice = { isWantingUpdate -> trackInAppUpdate(if (isWantingUpdate) "discoverNow" else "discoverLater") },
+            onInstallStart = { trackInAppUpdate("installUpdate") },
+            onInstallFailure = {
+                Sentry.captureException(it)
+                showSnackbar(title = R.string.errorUpdateInstall, anchor = getMainFab())
+            },
             onInAppUpdateUiChange = { isUpdateDownloaded ->
                 if (isUpdateDownloaded && canDisplayInAppSnackbar()) {
                     inAppUpdateSnackbar = showIndefiniteSnackbar(
                         title = R.string.updateReadyTitle,
                         actionButtonTitle = R.string.updateInstallButton,
                         anchor = getMainFab(),
-                        onActionClicked = ::installDownloadedUpdate,
+                        onActionClicked = inAppUpdateManager::installDownloadedUpdate,
                     )
                 } else if (!isUpdateDownloaded) {
                     inAppUpdateSnackbar?.dismiss()
                 }
-            }
+            },
             onFDroidResult = { updateIsAvailable ->
                 if (updateIsAvailable) navController.navigate(R.id.updateAvailableBottomSheetDialog)
-            }
-        }
+            },
+        )
     }
 
     private fun canDisplayInAppSnackbar() = inAppUpdateSnackbar?.isShown != true
