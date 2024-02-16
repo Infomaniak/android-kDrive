98c02973cd7f10477f73ee20f6cd8f076fd804f9
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-02-16 12:51:42 +0100

    Fix wrongly displayed navbar and missing small fab under some bottom sheets

--- app/src/main/java/com/infomaniak/drive/ui/MainActivity.kt
+++ app/src/main/java/com/infomaniak/drive/ui/MainActivity.kt
@@ -69,6 +69,7 @@ import com.infomaniak.drive.data.services.DownloadReceiver
 import com.infomaniak.drive.databinding.ActivityMainBinding
 import com.infomaniak.drive.ui.addFiles.AddFileBottomSheetDialogArgs
 import com.infomaniak.drive.ui.addFiles.UploadFilesHelper
+import com.infomaniak.drive.ui.bottomSheetDialogs.FileInfoActionsBottomSheetDialogArgs
 import com.infomaniak.drive.ui.fileList.FileListFragmentArgs
 import com.infomaniak.drive.utils.*
 import com.infomaniak.drive.utils.NavigationUiUtils.setupWithNavControllerCustom
@@ -350,6 +351,7 @@ class MainActivity : BaseActivity() {
         val shouldHideBottomNavigation = navigationArgs?.let(FileListFragmentArgs::fromBundle)?.shouldHideBottomNavigation
         val shouldShowSmallFab = navigationArgs?.let(FileListFragmentArgs::fromBundle)?.shouldShowSmallFab
             ?: navigationArgs?.let(AddFileBottomSheetDialogArgs::fromBundle)?.shouldShowSmallFab
+            ?: navigationArgs?.let(FileInfoActionsBottomSheetDialogArgs::fromBundle)?.shouldShowSmallFab
 
         handleBottomNavigationVisibility(destination.id, shouldHideBottomNavigation, shouldShowSmallFab)
 
@@ -414,13 +416,13 @@ class MainActivity : BaseActivity() {
     ) = with(binding) {
 
         val isVisible = when (destinationId) {
-            R.id.addFileBottomSheetDialog -> {
+            R.id.addFileBottomSheetDialog,
+            R.id.fileInfoActionsBottomSheetDialog,
+            R.id.fileListFragment -> {
                 val shouldHide = shouldHideBottomNavigation != false || shouldShowSmallFab == true
                 !shouldHide
             }
             R.id.favoritesFragment,
-            R.id.fileInfoActionsBottomSheetDialog,
-            R.id.fileListFragment,
             R.id.homeFragment,
             R.id.menuFragment,
             R.id.menuGalleryFragment,
