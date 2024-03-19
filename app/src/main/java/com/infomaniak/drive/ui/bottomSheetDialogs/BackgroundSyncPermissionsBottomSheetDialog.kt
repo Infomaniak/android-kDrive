6e3927b62c7599fb145c29a2ba5f8925335cd605
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-03-19 07:34:11 +0100

    Update all copyrights

--- app/src/main/java/com/infomaniak/drive/ui/bottomSheetDialogs/BackgroundSyncPermissionsBottomSheetDialog.kt
+++ app/src/main/java/com/infomaniak/drive/ui/bottomSheetDialogs/BackgroundSyncPermissionsBottomSheetDialog.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak kDrive - Android
- * Copyright (C) 2022 Infomaniak Network SA
+ * Copyright (C) 2022-2024 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.infomaniak.drive.ui.bottomSheetDialogs
 
 import android.content.DialogInterface
--- app/src/main/java/com/infomaniak/drive/ui/menu/RecyclerViewFastScroller.kt
+++ app/src/main/java/com/infomaniak/drive/ui/menu/RecyclerViewFastScroller.kt
@@ -1,11 +1,13 @@
 /*
+ * Infomaniak kDrive - Android
  * Copyright 2018 Quiph Media Pvt Ltd
+ * Copyright (C) 2022-2024 Infomaniak Network SA
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
