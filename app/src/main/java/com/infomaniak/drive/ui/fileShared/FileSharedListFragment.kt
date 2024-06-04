3217487a764bde7d43a6ac69a504b72dc4341594
Fabian Devel <fabian.devel@infomaniak.com>
2024-06-04 17:25:13 +0200

    feat(SharedLink): Add layout and empty classes of activity and its fragments

---
+++ app/src/main/java/com/infomaniak/drive/ui/fileShared/FileSharedListFragment.kt
@@ -0,0 +1,35 @@
+/*
+ * Infomaniak kDrive - Android
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.drive.ui.fileShared
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.infomaniak.lib.core.utils.safeBinding
+import com.infomaniak.drive.databinding.FragmentFileSharedListBinding
+
+class FileSharedListFragment: Fragment() {
+
+    private var binding: FragmentFileSharedListBinding by safeBinding()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return FragmentFileSharedListBinding.inflate(inflater, container, false).also { binding = it }.root
+    }
+}
