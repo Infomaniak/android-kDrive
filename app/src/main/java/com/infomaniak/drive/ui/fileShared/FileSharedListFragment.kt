c7285800e36f188a3dfaa04d19d61ba276deb507
Fabian Devel <fabian.devel@infomaniak.com>
2024-06-05 13:54:46 +0200

    feat(FileShare): Add viewModel and naive call to API

--- app/src/main/java/com/infomaniak/drive/ui/fileShared/FileSharedListFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/fileShared/FileSharedListFragment.kt
@@ -18,18 +18,35 @@
 package com.infomaniak.drive.ui.fileShared
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.infomaniak.lib.core.utils.safeBinding
+import androidx.fragment.app.activityViewModels
 import com.infomaniak.drive.databinding.FragmentFileSharedListBinding
+import com.infomaniak.lib.core.utils.safeBinding
 
-class FileSharedListFragment: Fragment() {
+class FileSharedListFragment : Fragment() {
 
     private var binding: FragmentFileSharedListBinding by safeBinding()
+    private val fileShareViewModel: FileSharedViewModel by activityViewModels()
+
+    private val fileSharedActivityArgs by lazy {
+        // When opening this fragment via deeplink, it can happen that the navigation
+        // extras aren't yet initialized, so we don't use the `navArgs` here.
+        requireActivity().intent?.extras?.let(FileSharedActivityArgs::fromBundle) ?: FileSharedActivityArgs()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return FragmentFileSharedListBinding.inflate(inflater, container, false).also { binding = it }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fileShareViewModel.downloadSharedFile().observe(viewLifecycleOwner) {
+            Log.e("TOTO", "onViewCreated: in Observer")
+        }
     }
 }
