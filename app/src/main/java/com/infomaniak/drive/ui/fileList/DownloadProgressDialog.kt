2b38c52629ba4ea65a478d7172a7ee34f934065c
Fabian Devel <fabian.devel@infomaniak.com>
2024-08-09 13:13:20 +0200

    feat(FileSharedActions): Add Base class for progress dialogs and a previewProgressDialog

--- app/src/main/java/com/infomaniak/drive/ui/fileList/DownloadProgressDialog.kt
+++ app/src/main/java/com/infomaniak/drive/ui/fileList/DownloadProgressDialog.kt
@@ -18,7 +18,6 @@
 package com.infomaniak.drive.ui.fileList
 
 import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
 import android.view.KeyEvent
 import androidx.fragment.app.DialogFragment
@@ -28,24 +27,18 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.infomaniak.drive.R
-import com.infomaniak.drive.data.api.ApiRoutes
 import com.infomaniak.drive.data.cache.FileController
 import com.infomaniak.drive.data.models.File
 import com.infomaniak.drive.data.models.UserDrive
 import com.infomaniak.drive.databinding.DialogDownloadProgressBinding
-import com.infomaniak.drive.utils.DownloadOfflineFileManager
-import com.infomaniak.drive.utils.IOFile
 import com.infomaniak.drive.utils.showSnackbar
 import com.infomaniak.lib.core.utils.setBackNavigationResult
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import okhttp3.Response
 
 class DownloadProgressDialog : DialogFragment() {
 
     private val binding: DialogDownloadProgressBinding by lazy { DialogDownloadProgressBinding.inflate(layoutInflater) }
     private val navigationArgs: DownloadProgressDialogArgs by navArgs()
-    private val downloadViewModel: DownloadViewModel by viewModels()
+    private val downloadProgressViewModel: DownloadProgressViewModel by viewModels()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = with(navigationArgs) {
         isCancelable = false
@@ -69,13 +62,13 @@ class DownloadProgressDialog : DialogFragment() {
     }
 
     private fun startDownloadFile(file: File, userDrive: UserDrive) {
-        downloadViewModel.downloadFile(requireContext(), file, userDrive)
+        downloadProgressViewModel.downloadFile(requireContext(), file, userDrive)
     }
 
     private fun observeDownloadedFile() = with(navigationArgs) {
-        downloadViewModel.downloadProgressLiveData.observe(this@DownloadProgressDialog) { progress ->
+        downloadProgressViewModel.downloadProgressLiveData.observe(this@DownloadProgressDialog) { progress ->
             progress?.let {
-                if (it == DownloadViewModel.PROGRESS_COMPLETE) {
+                if (it == DownloadProgressViewModel.PROGRESS_COMPLETE) {
                     setBackNavigationResult(action.value, fileId)
                 } else {
                     binding.downloadProgressIndicator.progress = progress
@@ -85,57 +78,6 @@ class DownloadProgressDialog : DialogFragment() {
                 findNavController().popBackStack()
             }
         }
-    }
-
-    class DownloadViewModel : ViewModel() {
-
-        val downloadProgressLiveData = MutableLiveData(0)
-
-        fun downloadFile(context: Context, file: File, userDrive: UserDrive) = viewModelScope.launch(Dispatchers.IO) {
-            val outputFile = file.getStoredFile(context, userDrive)
-            if (outputFile == null) {
-                downloadProgressLiveData.postValue(null)
-                return@launch
-            }
-            if (file.isObsoleteOrNotIntact(outputFile)) {
-                try {
-                    val response = DownloadOfflineFileManager.downloadFileResponse(
-                        fileUrl = ApiRoutes.downloadFile(file),
-                        downloadInterceptor = DownloadOfflineFileManager.downloadProgressInterceptor { progress ->
-                            downloadProgressLiveData.postValue(progress)
-                        }
-                    )
-                    if (response.isSuccessful) {
-                        saveData(file, outputFile, response)
-                    } else {
-                        downloadProgressLiveData.postValue(null)
-                    }
-                } catch (exception: Exception) {
-                    exception.printStackTrace()
-                    downloadProgressLiveData.postValue(null)
-                }
-            } else {
-                downloadProgressLiveData.postValue(PROGRESS_COMPLETE)
-            }
-        }
-
-        private fun saveData(file: File, outputFile: IOFile, response: Response) {
-            if (outputFile.exists()) outputFile.delete()
-            DownloadOfflineFileManager.saveRemoteData(TAG, response, outputFile) {
-                downloadProgressLiveData.postValue(PROGRESS_COMPLETE)
-            }
-            outputFile.setLastModified(file.getLastModifiedInMilliSecond())
-        }
-
-        companion object {
-            const val PROGRESS_COMPLETE = 100
-        }
-    }
-
-    enum class DownloadAction(val value: String) {
-        OPEN_WITH("open_with"),
-        OPEN_BOOKMARK("open_bookmark"),
-        PRINT_PDF("print_pdf"),
     }
 
     companion object {
