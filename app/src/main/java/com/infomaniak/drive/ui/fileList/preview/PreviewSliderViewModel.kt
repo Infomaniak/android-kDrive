ff9557c7a40a8afded57d060c972cd71b91c5bd2
Fabian Devel <fabian.devel@infomaniak.com>
2024-08-15 15:46:00 +0200

    feat(PublicShareBottomsheetAction): Add file actions

--- app/src/main/java/com/infomaniak/drive/ui/fileList/preview/PreviewSliderViewModel.kt
+++ app/src/main/java/com/infomaniak/drive/ui/fileList/preview/PreviewSliderViewModel.kt
@@ -17,60 +17,15 @@
  */
 package com.infomaniak.drive.ui.fileList.preview
 
-import android.content.Context
-import android.content.Intent
-import androidx.core.content.FileProvider
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.infomaniak.drive.R
 import com.infomaniak.drive.data.models.File
 import com.infomaniak.drive.data.models.UserDrive
-import com.infomaniak.drive.ui.fileList.BaseDownloadProgressDialog.DownloadAction
-import com.infomaniak.drive.utils.Utils.openWith
-import com.infomaniak.drive.utils.printPdf
-import com.infomaniak.drive.utils.saveToKDrive
-import com.infomaniak.drive.utils.shareFile
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class PreviewSliderViewModel : ViewModel() {
 
-    val downloadProgressLiveData = MutableLiveData(0)
     val pdfIsDownloading = MutableLiveData<Boolean>()
     var currentPreview: File? = null
     var userDrive = UserDrive()
     var publicShareUuid = ""
-
-    fun executeDownloadAction(
-        activityContext: Context,
-        downloadAction: DownloadAction,
-        navigateToDownloadDialog: suspend () -> Unit,
-        onDownloadError: () -> Unit,
-    ) = viewModelScope.launch(Dispatchers.IO) {
-        runCatching {
-            val cacheFile = currentPreview!!.convertToIOFile(
-                context = activityContext,
-                userDrive = userDrive,
-                onProgress = downloadProgressLiveData::postValue,
-                navigateToDownloadDialog = navigateToDownloadDialog,
-            )
-
-            val uri = FileProvider.getUriForFile(activityContext, activityContext.getString(R.string.FILE_AUTHORITY), cacheFile)
-
-            when (downloadAction) {
-                DownloadAction.OPEN_WITH -> {
-                    activityContext.openWith(uri, currentPreview!!.getMimeType(), Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                }
-                DownloadAction.SEND_COPY -> activityContext.shareFile { uri }
-                DownloadAction.SAVE_TO_DRIVE -> activityContext.saveToKDrive(uri)
-                DownloadAction.OPEN_BOOKMARK -> TODO()
-                DownloadAction.PRINT_PDF -> activityContext.printPdf(cacheFile)
-            }
-        }.onFailure { exception ->
-            downloadProgressLiveData.postValue(null)
-            exception.printStackTrace()
-            onDownloadError()
-        }
-    }
 }
