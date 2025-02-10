bc26cc0b9cb5be7037c77763f6222066f62c604d
Vincent TE <vincent.te@infomaniak.com>
2025-02-10 12:59:56 +0100

    chore: Clean code

--- app/src/main/java/com/infomaniak/drive/ui/BasePreviewSliderFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/BasePreviewSliderFragment.kt
@@ -91,10 +91,11 @@ abstract class BasePreviewSliderFragment : Fragment(), FileInfoActionsView.OnIte
 
     private val mainExecutor by lazy { ContextCompat.getMainExecutor(requireContext()) }
 
+    var positionForMedium: MutableMap<Int?, Long?> = mutableMapOf()
+
     private var mediaControllerFuture: ListenableFuture<MediaController>? = null
     private var mediaController: MediaController? = null
 
-    var positionForMedium: MutableMap<Int?, Long?> = mutableMapOf()
     // If the user click want to navigate back and something is playing, we don't want to start PIP
     private var canStartPictureInPicture = true
 
