da4a01d91d25d92af9e5f4e4147a5d69a404d7fe
Morgane FAREZ <morgane.farez@infomaniak.com>
2025-06-03 14:18:42 +0200

    refactor: Extract MediaFolders VM

--- app/src/main/java/com/infomaniak/drive/ui/menu/settings/SelectMediaFoldersDialog.kt
+++ app/src/main/java/com/infomaniak/drive/ui/menu/settings/SelectMediaFoldersDialog.kt
@@ -17,7 +17,6 @@
  */
 package com.infomaniak.drive.ui.menu.settings
 
-import android.content.ContentResolver
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -27,26 +26,20 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.*
 import com.infomaniak.drive.R
-import com.infomaniak.drive.data.models.MediaFolder
 import com.infomaniak.drive.databinding.FragmentBottomSheetSelectMediaFoldersBinding
 import com.infomaniak.drive.utils.DrivePermissions
-import com.infomaniak.drive.utils.IsComplete
-import com.infomaniak.drive.utils.MediaFoldersProvider
 import com.infomaniak.drive.views.FullScreenBottomSheetDialog
 import com.infomaniak.drive.views.NoItemsLayoutView
 import com.infomaniak.lib.core.utils.safeBinding
 import com.infomaniak.lib.core.views.DividerItemDecorator
-import io.realm.Realm
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runInterruptible
 
 class SelectMediaFoldersDialog : FullScreenBottomSheetDialog(), NoItemsLayoutView.INoItemsLayoutView {
 
     private var binding: FragmentBottomSheetSelectMediaFoldersBinding by safeBinding()
 
-    private val mediaViewModel: MediaViewModel by viewModels()
+    private val mediaViewModel: SelectMediaFoldersViewModel by viewModels()
     private lateinit var mediaFoldersAdapter: MediaFoldersAdapter
 
     override val noItemsIcon = R.drawable.ic_folder_filled
@@ -110,60 +103,5 @@ class SelectMediaFoldersDialog : FullScreenBottomSheetDialog(), NoItemsLayoutVie
     override fun onDismiss(dialog: DialogInterface) {
         super.onDismiss(dialog)
         (requireActivity() as SyncSettingsActivity).onDialogDismissed()
-    }
-
-    class MediaViewModel : ViewModel() {
-
-        private var getMediaFilesJob: Job = Job()
-        val elementsToRemove = MutableLiveData<List<Long>>()
-
-        fun getAllMediaFolders(contentResolver: ContentResolver): LiveData<Pair<IsComplete, ArrayList<MediaFolder>>> {
-            getMediaFilesJob = Job()
-            return liveData {
-                runInterruptible(Dispatchers.IO + getMediaFilesJob) {
-                    MediaFolder.getRealmInstance().use { realm ->
-                        val cacheMediaFolders = MediaFolder.getAll(realm)
-                        viewModelScope.launch(Dispatchers.Main) {
-                            if (cacheMediaFolders.isNotEmpty()) emit(false to cacheMediaFolders)
-                        }
-
-                        val localMediaFolders = ArrayList(
-                            MediaFoldersProvider.getAllMediaFolders(realm, contentResolver, getMediaFilesJob),
-                        )
-                        cacheMediaFolders.removeObsoleteMediaFolders(realm, localMediaFolders.map { it.id })
-
-                        viewModelScope.launch(Dispatchers.Main) {
-                            emit(true to localMediaFolders.newMediaFolders(cacheMediaFolders))
-                        }
-                    }
-                }
-            }
-        }
-
-        override fun onCleared() {
-            getMediaFilesJob.cancel()
-            super.onCleared()
-        }
-
-        private fun ArrayList<MediaFolder>.newMediaFolders(cachedMediaFolders: ArrayList<MediaFolder>): ArrayList<MediaFolder> {
-            return filterNot { mediaFolder ->
-                cachedMediaFolders.any { cachedFolder -> cachedFolder.id == mediaFolder.id }
-            } as ArrayList<MediaFolder>
-        }
-
-        private fun List<MediaFolder>.removeObsoleteMediaFolders(realm: Realm, upToDateMediasIds: List<Long>) {
-            val deletedMediaFolderList = mutableListOf<Long>()
-
-            realm.executeTransaction { currentRealm ->
-                forEach { cachedFile ->
-                    if (!upToDateMediasIds.contains(cachedFile.id)) {
-                        MediaFolder.delete(currentRealm, cachedFile.id)
-                        deletedMediaFolderList.add(cachedFile.id)
-                    }
-                }
-            }
-
-            if (deletedMediaFolderList.isNotEmpty()) elementsToRemove.postValue(deletedMediaFolderList)
-        }
     }
 }
