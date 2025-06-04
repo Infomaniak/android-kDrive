e1acda41e06386153cff5f8eb4eead7067bc2b9a
Louis CAD <louis.cognault@infomaniak.com>
2025-06-04 15:28:17 +0200

    feat(CrossAppLogin): Implement and declare CrossAppLoginService

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -151,6 +151,7 @@ sentry {
 dependencies {
 
     implementation(project(":Core"))
+    implementation(project(":Core:CrossAppLogin"))
     implementation(project(":Core:FragmentNavigation"))
     implementation(project(":Core:Ktor"))
     implementation(project(":Core:Legacy"))
