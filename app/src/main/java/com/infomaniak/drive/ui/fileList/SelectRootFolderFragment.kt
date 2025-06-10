c2cd2e034a136762054c2bd20de4088a099f063d
Kylian Grandy <kylian.grandy@infomaniak.com>
2025-06-10 08:20:54 +0200

    feat: Display view when move + recent folder display function (test)
    - Add recent view, non-functional part
    - Add root folders, add root folder navigation

--- app/src/main/java/com/infomaniak/drive/ui/fileList/SelectRootFolderFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/fileList/SelectRootFolderFragment.kt
@@ -21,29 +21,140 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import com.infomaniak.drive.R
+import com.infomaniak.drive.data.models.File
+import com.infomaniak.drive.data.models.UiSettings
+import com.infomaniak.drive.databinding.CardviewFileListBinding
 import com.infomaniak.drive.databinding.FragmentSelectRootFolderBinding
 import com.infomaniak.drive.extensions.enableEdgeToEdge
+import com.infomaniak.drive.ui.home.RootFileTreeCategory.*
+import com.infomaniak.drive.ui.home.RootFilesFragment.FolderToOpen
+import com.infomaniak.drive.utils.AccountUtils
 import com.infomaniak.lib.core.utils.safeBinding
+import com.infomaniak.lib.core.utils.safeNavigate
+import com.infomaniak.lib.core.utils.setMargins
+import kotlinx.coroutines.CompletableJob
+import kotlinx.coroutines.Job
 
 class SelectRootFolderFragment : Fragment() {
 
     private var binding: FragmentSelectRootFolderBinding by safeBinding()
+    private val fileListViewModel: FileListViewModel by viewModels()
+
+    private val uiSettings by lazy { UiSettings(requireContext()) }
+
+    private var commonFolderToOpen: FolderToOpen? = null
+    private var personalFolderToOpen: FolderToOpen? = null
+    private val hasFolderToOpenBeenSet: CompletableJob = Job()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return FragmentSelectRootFolderBinding.inflate(inflater, container, false).also { binding = it }.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.toolbar.setNavigationOnClickListener {
+        collapsingToolbarLayout.title = "Sélectionner un dossier"
+
+        val currentDrive = AccountUtils.getCurrentDrive(forceRefresh = true)
+        rootFolderTitle.text = currentDrive?.name
+
+        rootFolderLayout.recentChanges.isGone = true
+        rootFolderLayout.offlineFile.isGone = true
+        rootFolderLayout.trashbin.isGone = true
+
+        setupRecentFolderView()
+
+        with((activity as SelectFolderActivity).getSaveButton()) {
+            isGone = true
+        }
+
+        setupItems()
+
+        observeFiles()
+
+        toolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
         }
 
-        binding.root.enableEdgeToEdge()
+        binding.root.enableEdgeToEdge(withBottom = false) { windowInsets ->
+            binding.rootFolderLayout.cardView.setMargins(
+                bottom = resources.getDimension(R.dimen.recyclerViewPaddingBottom).toInt() + windowInsets.bottom
+            )
+        }
     }
 
+    private fun setupRecentFolderView() {
+        // Test function
+        binding.recentFolderTitle.isVisible = true
+        val container: LinearLayout = binding.recentListLayout
 
+        val folder1Binding = CardviewFileListBinding.inflate(layoutInflater)
+        val folder2Binding = CardviewFileListBinding.inflate(layoutInflater)
+        val folder3Binding = CardviewFileListBinding.inflate(layoutInflater)
+
+        container.addView(folder1Binding.root, 0)
+        container.addView(folder2Binding.root, 1 )
+        container.addView(folder3Binding.root, 2)
+    }
+
+    private fun setupItems() = with(binding) {
+        rootFolderLayout.organizationFolder.setOnClickListener {
+            uiSettings.lastVisitedRootFileTreeCategory = CommonFolders
+            commonFolderToOpen?.let { safeNavigate(fileListDirections(it)) }
+        }
+
+        rootFolderLayout.personalFolder.setOnClickListener {
+            uiSettings.lastVisitedRootFileTreeCategory = PersonalFolder
+            personalFolderToOpen?.let { safeNavigate(fileListDirections(it)) }
+        }
+
+        rootFolderLayout.favorites.setOnClickListener {
+            uiSettings.lastVisitedRootFileTreeCategory = Favorites
+            safeNavigate(SelectRootFolderFragmentDirections.actionSelectRootFolderFragmentToFavoritesFragment())
+        }
+
+        rootFolderLayout.sharedWithMeFiles.setOnClickListener {
+            uiSettings.lastVisitedRootFileTreeCategory = SharedWithMe
+            safeNavigate(SelectRootFolderFragmentDirections.actionSelectRootFolderFragmentToSharedWithMeFragment())
+        }
+
+        rootFolderLayout. myShares.setOnClickListener {
+            uiSettings.lastVisitedRootFileTreeCategory = MyShares
+            safeNavigate(SelectRootFolderFragmentDirections.actionSelectRootFolderFragmentToMySharesFragment())
+        }
+    }
+
+    private fun observeFiles() {
+        fileListViewModel.rootFiles.observe(viewLifecycleOwner) { fileTypes ->
+            binding.rootFolderLayout.organizationFolder.isVisible = fileTypes.contains(File.VisibilityType.IS_TEAM_SPACE)
+            binding.rootFolderLayout.personalFolder.isVisible = fileTypes.contains(File.VisibilityType.IS_PRIVATE)
+
+            updateFolderToOpenWhenClicked(fileTypes)
+        }
+    }
+
+    private fun updateFolderToOpenWhenClicked(fileTypes: Map<File.VisibilityType, File>) {
+        fileTypes[File.VisibilityType.IS_TEAM_SPACE]?.let { file ->
+            commonFolderToOpen = FolderToOpen(file.id, file.getDisplayName(requireContext()))
+        }
+        fileTypes[File.VisibilityType.IS_PRIVATE]?.let { file ->
+            personalFolderToOpen = FolderToOpen(file.id, file.getDisplayName(requireContext()))
+        }
+        hasFolderToOpenBeenSet.complete()
+    }
+
+    private fun fileListDirections(
+        folderToOpen: FolderToOpen,
+    ): NavDirections = SelectRootFolderFragmentDirections.selectRootFolderFragmentToSelectFolderFragment(
+        folderId = folderToOpen.id,
+        folderName = folderToOpen.name
+    )
 }
