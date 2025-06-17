2eef3b69fcacd81a9829b2a8a9f525e043f88975
Louis CAD <louis.cognault@infomaniak.com>
2025-06-17 15:27:15 +0200

    fix(CrossAppLogin): Provide a looper in the single thread dispatcher used for Realm

--- app/src/main/java/com/infomaniak/drive/data/models/AppSettings.kt
+++ app/src/main/java/com/infomaniak/drive/data/models/AppSettings.kt
@@ -18,6 +18,7 @@
 package com.infomaniak.drive.data.models
 
 import com.infomaniak.core.flowOnLazyClosable
+import com.infomaniak.drive.extensions.HandlerThreadDispatcher
 import com.infomaniak.drive.utils.RealmModules
 import io.realm.DynamicRealm
 import io.realm.Realm
@@ -84,7 +85,7 @@ open class AppSettings(
             emitAll(flow)
         }.flowOnLazyClosable {
             @OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
-            newSingleThreadContext("RealmDispatcher")
+            HandlerThreadDispatcher("Realm-currentUserIdFlow")
         }
 
         fun updateAppSettings(onUpdate: (appSettings: AppSettings) -> Unit) {
