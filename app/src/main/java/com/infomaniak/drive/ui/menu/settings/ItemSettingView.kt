75101a2ee3fef7e6bb9e0d8771eed6309bc4260f
Morgane FAREZ <morgane.farez@infomaniak.com>
2025-06-30 13:15:11 +0200

    refactor: Rearrange and clean the views

--- app/src/main/java/com/infomaniak/drive/ui/menu/settings/ItemSettingView.kt
+++ app/src/main/java/com/infomaniak/drive/ui/menu/settings/ItemSettingView.kt
@@ -23,6 +23,7 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.CompoundButton
 import android.widget.FrameLayout
+import androidx.annotation.ColorInt
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import com.infomaniak.drive.R
@@ -84,7 +85,7 @@ class ItemSettingView @JvmOverloads constructor(
         binding.toggle.setOnCheckedChangeListener(listener)
     }
 
-    fun setIconColor(color: Int) {
+    fun setIconColor(@ColorInt color: Int) {
         binding.icon.imageTintList = ColorStateList.valueOf(color)
     }
 
