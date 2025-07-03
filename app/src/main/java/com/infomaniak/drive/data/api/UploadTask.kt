c7a737be4221330357160cf7e94914c2b29fc82a
Louis CAD <louis.cognault@infomaniak.com>
2025-07-03 10:25:17 +0200

    chore: Lower level of Content-Length/body size mismatch from error to warning

--- app/src/main/java/com/infomaniak/drive/data/api/UploadTask.kt
+++ app/src/main/java/com/infomaniak/drive/data/api/UploadTask.kt
@@ -350,6 +350,7 @@ class UploadTask(
                 if (expectedContentLength != bytes.size) Sentry.withScope { scope ->
                     scope.setExtra("contentLength", expectedContentLength.toString())
                     scope.setExtra("received", bytes.size.toString())
+                    scope.level = SentryLevel.WARNING
                     Sentry.captureMessage("Backend provided more or fewer bytes than the contentLength it declared!")
                 }
             }
