effe489134ea1b0c937feee6ad8161c966605f39
Morgane FAREZ <morgane.farez@infomaniak.com>
2025-07-08 11:17:30 +0200

    refactor: Extract sentry configuration into a method and clean comments

--- app/src/main/java/com/infomaniak/drive/MainApplication.kt
+++ app/src/main/java/com/infomaniak/drive/MainApplication.kt
@@ -109,20 +109,7 @@ class MainApplication : Application(), ImageLoaderFactory, DefaultLifecycleObser
         }
 
         ApiController.init(typeAdapterList = arrayListOf(File::class.java to FileDeserialization()))
-
-        /**
-         * Reasons to discard Sentry events :
-         * - Application is in Debug mode
-         * - User deactivated Sentry tracking in DataManagement settings
-         * - The exception was an [ApiController.NetworkException], and we don't want to send them to Sentry
-         */
-        this.configureSentry(
-            BuildConfig.DEBUG,
-            // TODO: Update isSentryTrackingEnabled with the sharedpreference value
-            true,
-            // TODO: Check if there are exceptions to manage
-            { _: Throwable? -> false })
-
+        configSentry()
         runBlocking { initRealm() }
 
         geniusScanIsReady = initGeniusScanSdk()
@@ -208,5 +195,19 @@ class MainApplication : Application(), ImageLoaderFactory, DefaultLifecycleObser
         override suspend fun getApiToken(): ApiToken? = AccountUtils.currentUser?.apiToken
 
         override fun getCurrentUserId(): Int = AccountUtils.currentUserId
+    }
+
+    /**
+     * Reasons to discard Sentry events :
+     * - Application is in Debug mode
+     * - User deactivated Sentry tracking in DataManagement settings
+     * - The exception was an [ApiController.NetworkException], and we don't want to send them to Sentry
+     */
+    private fun configSentry() {
+        this.configureSentry(
+            isDebug = BuildConfig.DEBUG,
+            isSentryTrackingEnabled = true, // TODO: Update isSentryTrackingEnabled with the sharedpreference value when the Sentry opt-out will be enabled
+            isErrorException = { _: Throwable? -> false },
+        )
     }
 }
