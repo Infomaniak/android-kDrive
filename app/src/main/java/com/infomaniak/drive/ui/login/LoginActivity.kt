0aebc8438c24471016f4aee2bb1306c7238b38a7
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2025-07-08 16:13:10 +0200

    Add data to help test the UI

--- app/src/main/java/com/infomaniak/drive/ui/login/LoginActivity.kt
+++ app/src/main/java/com/infomaniak/drive/ui/login/LoginActivity.kt
@@ -37,6 +37,7 @@ import androidx.viewpager2.widget.ViewPager2
 import com.infomaniak.core.Xor
 import com.infomaniak.core.cancellable
 import com.infomaniak.core.crossloginui.data.CrossLoginAccount
+import com.infomaniak.core.crossloginui.previews.accountsPreviewData
 import com.infomaniak.core.utils.awaitOneClick
 import com.infomaniak.drive.BuildConfig
 import com.infomaniak.drive.MatomoDrive.trackAccountEvent
@@ -246,7 +247,23 @@ class LoginActivity : AppCompatActivity() {
 
             loginViewModel.initDerivedTokenGenerator(coroutineScope = this)
 
-            val accounts = loginViewModel.getCrossLoginAccounts(context = this@LoginActivity)
+            // val accounts = loginViewModel.getCrossLoginAccounts(context = this@LoginActivity)
+            // TODO: retirer le code de test
+            val accounts = loginViewModel.getCrossLoginAccounts(context = this@LoginActivity).toMutableList().apply {
+                addAll(
+                    accountsPreviewData.map {
+                        CrossLoginAccount(
+                            tokens = setOf("I'm a token"),
+                            isCurrentlySelectedInAnApp = false,
+                            id = it.id,
+                            name = it.name,
+                            initials = it.initials,
+                            email = it.email,
+                            url = it.url,
+                        )
+                    },
+                )
+            }
 
             if (accounts.isNotEmpty()) {
                 loginViewModel.crossLoginAccounts.value = accounts
