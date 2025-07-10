7297f7ef568f1ac7ed9212555c69f2a9e550fd3c
Vincent TE <vincent.te@infomaniak.com>
2025-07-10 09:15:25 +0200

    chore: Fix Sonar lint

--- app/src/main/java/com/infomaniak/drive/data/sync/UploadNotifications.kt
+++ app/src/main/java/com/infomaniak/drive/data/sync/UploadNotifications.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak kDrive - Android
- * Copyright (C) 2022-2024 Infomaniak Network SA
+ * Copyright (C) 2022-2025 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -197,7 +197,7 @@ object UploadNotifications {
         var folderId: Int = resources.remoteFolder
         if (matches != null) {
             for (match in matches) {
-                var childrenId = FileController.getIdOfChildrenFileWithName(folderId, match)
+                val childrenId = FileController.getIdOfChildrenFileWithName(folderId, match)
                 if (childrenId.isNotEmpty()) {
                     folderId = childrenId.first()
                 } else {
