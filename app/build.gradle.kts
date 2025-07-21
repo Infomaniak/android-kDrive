e2b1c078122e426397abf13a11f155204fa16244
Morgane FAREZ <morgane.farez@infomaniak.com>
2025-07-21 08:44:08 +0200

    refactor: Clean code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -164,8 +164,8 @@ dependencies {
     implementation(project(":Core:Matomo"))
     implementation(project(":Core:MyKSuite"))
     implementation(project(":Core:Network"))
-    implementation(project(":Core:Sentry"))
     implementation(project(":Core:RecyclerView"))
+    implementation(project(":Core:Sentry"))
     implementation(project(":Core:Thumbnails"))
 
     implementation(core.ktor.client.okhttp)
