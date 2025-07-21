4670c5f044f2f737bf43ef6f71ca157e6e047ef2
Morgane FAREZ <morgane.farez@infomaniak.com>
2025-07-21 11:27:43 +0200

    refactor: Use the default value of isFilteredException

--- app/src/main/java/com/infomaniak/drive/MainApplication.kt
+++ app/src/main/java/com/infomaniak/drive/MainApplication.kt
@@ -226,7 +226,6 @@ class MainApplication : Application(), ImageLoaderFactory, DefaultLifecycleObser
         configureSentry(
             isDebug = BuildConfig.DEBUG,
             isSentryTrackingEnabled = UiSettings(applicationContext) .isSentryTrackingEnabled,
-            isFilteredException = { _: Throwable? -> false },
         )
     }
 }
