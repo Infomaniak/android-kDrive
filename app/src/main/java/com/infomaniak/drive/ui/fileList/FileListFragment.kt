5d857abf77a8b6bd251a29e00c973fb3f9010edd
Kylian Grandy <kylian.grandy@infomaniak.com>
2025-08-06 13:09:51 +0200

    refactor: Apply sonarqube review

--- app/src/main/java/com/infomaniak/drive/ui/fileList/FileListFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/fileList/FileListFragment.kt
@@ -496,9 +496,7 @@ open class FileListFragment : MultiSelectFragment(
 
             onEmptyList = { checkIfNoFiles() }
 
-            if (userDrive != null && userDrive?.userId != AccountUtils.currentUserId) {
-                mainApp.newImageLoader(userDrive?.userId)
-            }
+            newImageLoader = mainApp.newImageLoader(userDrive?.userId)
 
             onFileClicked = getFunctionByFileType()
 
