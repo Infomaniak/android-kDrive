0f4e48e1bc68cc1d7101437ba47cad084343e65c
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2025-08-21 07:04:48 +0200

    update drive object

--- app/src/main/java/com/infomaniak/drive/utils/KSuiteNavUtils.kt
+++ app/src/main/java/com/infomaniak/drive/utils/KSuiteNavUtils.kt
@@ -17,12 +17,14 @@
  */
 package com.infomaniak.drive.utils
 
-import android.content.Context
+import android.app.Activity
 import android.graphics.Color
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.infomaniak.core.avatar.getBackgroundColorResBasedOnId
+import com.infomaniak.core.fragmentnavigation.safelyNavigate
+import com.infomaniak.core.ksuite.data.KSuite
 import com.infomaniak.core.ksuite.myksuite.ui.data.MyKSuiteData
 import com.infomaniak.core.ksuite.myksuite.ui.screens.KSuiteApp
 import com.infomaniak.core.ksuite.myksuite.ui.screens.MyKSuiteDashboardScreenData
@@ -30,13 +32,16 @@ import com.infomaniak.core.ksuite.myksuite.ui.utils.MatomoMyKSuite
 import com.infomaniak.core.ksuite.myksuite.ui.utils.MyKSuiteUiUtils
 import com.infomaniak.core.ksuite.myksuite.ui.utils.MyKSuiteUiUtils.openMyKSuiteUpgradeBottomSheet
 import com.infomaniak.drive.MatomoDrive.trackEvent
+import com.infomaniak.drive.MatomoDrive.trackKSuiteProBottomSheetEvent
+import com.infomaniak.drive.R
+import com.infomaniak.drive.ui.bottomSheetDialogs.KSuiteProBottomSheetDialogArgs
 import com.infomaniak.lib.core.models.user.User
 
 fun Fragment.openMyKSuiteUpgradeBottomSheet(matomoTrackerName: String) {
-    requireActivity().openMyKSuiteUpgradeBottomSheet(findNavController(), matomoTrackerName)
+    openMyKSuiteUpgradeBottomSheet(findNavController(), matomoTrackerName)
 }
 
-fun Context.openMyKSuiteUpgradeBottomSheet(navController: NavController, matomoTrackerName: String) {
+fun openMyKSuiteUpgradeBottomSheet(navController: NavController, matomoTrackerName: String) {
     trackEvent(MatomoMyKSuite.CATEGORY_MY_KSUITE_UPGRADE_BOTTOMSHEET, matomoTrackerName)
     navController.openMyKSuiteUpgradeBottomSheet(KSuiteApp.Drive)
 }
@@ -50,5 +55,31 @@ fun Fragment.getDashboardData(myKSuiteData: MyKSuiteData, user: User): MyKSuiteD
         userInitials = user.getInitials(),
         iconColor = Color.WHITE,
         userInitialsBackgroundColor = requireContext().getBackgroundColorResBasedOnId(user.id.hashCode()),
+    )
+}
+
+fun Fragment.openKSuiteProBottomSheet(
+    kSuite: KSuite,
+    isAdmin: Boolean,
+    matomoTrackerName: String,
+) {
+    trackKSuiteProBottomSheetEvent(matomoTrackerName)
+    safelyNavigate(
+        resId = R.id.kSuiteProBottomSheetDialog,
+        args = KSuiteProBottomSheetDialogArgs(kSuite, isAdmin).toBundle(),
+    )
+}
+
+fun Activity.openKSuiteProBottomSheet(
+    navController: NavController,
+    kSuite: KSuite,
+    isAdmin: Boolean,
+    matomoTrackerName: String,
+) {
+    trackKSuiteProBottomSheetEvent(matomoTrackerName)
+    safelyNavigate(
+        navController = navController,
+        resId = R.id.kSuiteProBottomSheetDialog,
+        args = KSuiteProBottomSheetDialogArgs(kSuite, isAdmin).toBundle(),
     )
 }
