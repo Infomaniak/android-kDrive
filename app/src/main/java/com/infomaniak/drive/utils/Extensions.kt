5adc983c72819745dac23f6c1768479f46fb38f5
Vincent TE <vincent.cheun.ang.te@gmail.com>
2025-10-01 13:24:26 +0200

    fix: Remove toggleEdgeToEdge since we handle EdgeToEdge

--- app/src/main/java/com/infomaniak/drive/utils/Extensions.kt
+++ app/src/main/java/com/infomaniak/drive/utils/Extensions.kt
@@ -114,6 +114,16 @@ import com.infomaniak.drive.utils.FilePresenter.openFolder
 import com.infomaniak.drive.utils.Utils.OTHER_ROOT_ID
 import com.infomaniak.drive.utils.Utils.Shortcuts
 import com.infomaniak.drive.views.PendingFilesView
+import com.infomaniak.lib.core.models.ApiResponse
+import com.infomaniak.lib.core.models.user.User
+import com.infomaniak.lib.core.utils.SnackbarUtils.showSnackbar
+import com.infomaniak.lib.core.utils.UtilsUi.openUrl
+import com.infomaniak.lib.core.utils.isNightModeEnabled
+import com.infomaniak.lib.core.utils.context
+import com.infomaniak.lib.core.utils.lightNavigationBar
+import com.infomaniak.lib.core.utils.lightStatusBar
+import com.infomaniak.lib.core.utils.loadAvatar
+import com.infomaniak.lib.core.utils.safeNavigate
 import com.infomaniak.lib.login.InfomaniakLogin
 import handleActionDone
 import io.realm.RealmList
