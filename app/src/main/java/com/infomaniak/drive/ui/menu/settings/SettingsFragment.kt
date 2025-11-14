2a525022e116b70d23b1062a697c52c6afa231a6
Jamy Bailly <jamy.bailly@infomaniak.com>
2025-11-14 10:01:21 +0100

    feat: Use AppVersionChecker instead of legacy GithubTool on BugTracker

--- app/src/main/java/com/infomaniak/drive/ui/menu/settings/SettingsFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/menu/settings/SettingsFragment.kt
@@ -204,10 +204,10 @@ class SettingsFragment : Fragment() {
                 putExtras(
                     BugTrackerActivityArgs(
                         user = AccountUtils.currentUser!!,
+                        appId = BuildConfig.APPLICATION_ID,
                         appBuildNumber = BuildConfig.VERSION_NAME,
                         bucketIdentifier = BuildConfig.BUGTRACKER_DRIVE_BUCKET_ID,
                         projectName = BuildConfig.BUGTRACKER_DRIVE_PROJECT_NAME,
-                        repoGitHub = BuildConfig.GITHUB_REPO,
                     ).toBundle(),
                 )
             }.also(::startActivity)
