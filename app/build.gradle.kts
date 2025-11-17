d04421082fed1d3328ded9d70ed30633af68b46a
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-11-17 15:38:14 +0100

    chore(Version): 5.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.infomaniak.drive"
         minSdk = appMinSdk
         targetSdk = appTargetSdk
-        versionCode = 5_010_005_01
-        versionName = "5.10.5"
+        versionCode = 5_011_000_01
+        versionName = "5.11.0"
 
         setProperty("archivesBaseName", "kdrive-$versionName ($versionCode)")
 
