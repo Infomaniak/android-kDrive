Infomaniak/android-kDrive

commit 1e0d047b3eabad1d4cfea8c15685d9acefce691a

fix: Delete file in realm when the file has been uploaded with success

app/src/main/java/com/infomaniak/drive/data/services/UploadWorker.kt
@@ -332,9 +332,7 @@ class UploadWorker(appContext: Context, params: WorkerParameters) : CoroutineWor
         return UploadTask(context = applicationContext, uploadFile = this, setProgress = ::setProgress).run {
             currentUploadTask = this
             start().also { isUploaded ->
-                if (isUploaded && UploadFile.getAppSyncSettings()?.deleteAfterSync != true) {
-                    deleteIfExists(keepFile = isSync())
-                }
+                if (isUploaded) deleteIfExists(keepFile = isSync())
 
                 SentryLog.d(TAG, "startUploadFile> end upload file")
             }

commit 292406a229693480f2b6e60ab648b092e2c13926

fix: Fix deleteAfterSync
Fix the issue introduced in 1e0d047b

app/src/main/java/com/infomaniak/drive/data/services/UploadWorker.kt
@@ -338,7 +338,11 @@ class UploadWorker(appContext: Context, params: WorkerParameters) : CoroutineWor
         return UploadTask(context = applicationContext, uploadFile = this, setProgress = ::setProgress).run {
             currentUploadTask = this
             start().also { isUploaded ->
-                if (isUploaded) deleteIfExists(keepFile = isSync())
+                if (isUploaded) {
+                    // If the below is true, will be deleted after the user confirms pictures deletion.
+                    val doNotDeleteNow = UploadFile.getAppSyncSettings()?.deleteAfterSync == true && isSync()
+                    if (!doNotDeleteNow) deleteIfExists(keepFile = isSync())
+                }
 
                 SentryLog.d(TAG, "startUploadFile> end upload file")
             }