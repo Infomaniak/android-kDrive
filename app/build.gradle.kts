9100f42acc03f02cdf9d55c463bc684a84e9b38b
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2025-12-23 13:16:08 +0100

    chore: New Release 5.11.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.infomaniak.drive"
         minSdk = appMinSdk
         targetSdk = appTargetSdk
-        versionCode = 5_011_005_01
-        versionName = "5.11.5"
+        versionCode = 5_011_006_01
+        versionName = "5.11.6"
 
         setProperty("archivesBaseName", "kdrive-$versionName ($versionCode)")
 
