bf58b0e0933964c1cc8fe2a1be6a763726d80caf
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-01-15 15:53:45 +0100

    feat: Add onlyWifiSyncMedia in app setting

--- app/src/main/java/com/infomaniak/drive/data/models/AppSettings.kt
+++ app/src/main/java/com/infomaniak/drive/data/models/AppSettings.kt
@@ -41,6 +41,7 @@ open class AppSettings(
     var _currentDriveId: Int = -1,
     var _currentUserId: Int = -1,
     var _onlyWifiSyncOffline: Boolean = false,
+    var _onlyWifiSyncMedia: Boolean = false,
 ) : RealmObject() {
 
     fun update(appSettings: AppSettings) {
@@ -112,6 +113,12 @@ open class AppSettings(
                 field = value
                 updateAppSettings { _onlyWifiSyncOffline = value }
             }
+
+        var onlyWifiSyncMedia: Boolean = getAppSettings()._onlyWifiSyncMedia
+            set(value) {
+                field = value
+                updateAppSettings { _onlyWifiSyncMedia = value }
+            }
     }
 
     class AppSettingsMigration : RealmMigration {
@@ -127,6 +134,7 @@ open class AppSettings(
                     }
                     if (oldVersion < 3L) {
                         renameField("_onlyWifiSync", "_onlyWifiSyncOffline")
+                        addField("_onlyWifiSyncMedia", Boolean::class.java, FieldAttribute.REQUIRED)
                     }
                 }
             }
