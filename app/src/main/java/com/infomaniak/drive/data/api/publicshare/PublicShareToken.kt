d6c6a5174e25a67575bcc288ee6e1b3836313428
Fabian DEVEL <fabian.devel@infomaniak.com>
2026-01-16 14:58:20 +0100

    chore(PublicShare): Add missing commas

--- app/src/main/java/com/infomaniak/drive/data/api/publicshare/PublicShareToken.kt
+++ app/src/main/java/com/infomaniak/drive/data/api/publicshare/PublicShareToken.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak kDrive - Android
- * Copyright (C) 2025 Infomaniak Network SA
+ * Copyright (C) 2025-2026 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,5 +22,5 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class PublicShareToken(
-    val token: String = ""
+    val token: String = "",
 ) : Parcelable
