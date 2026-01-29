b36c89c4553f33bcf0e8ba5618f533d93aedb57b
Fabian DEVEL <fabian.devel@infomaniak.com>
2026-01-29 15:11:36 +0100

    fix(Search): Fix the stableIds crash in the search

--- app/src/main/java/com/infomaniak/drive/ui/fileList/FileListFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/fileList/FileListFragment.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak kDrive - Android
- * Copyright (C) 2022-2025 Infomaniak Network SA
+ * Copyright (C) 2022-2026 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -526,7 +526,7 @@ open class FileListFragment : MultiSelectFragment(
             lifecycle = viewLifecycleOwner.lifecycle,
         ).apply {
             stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
-            setHasStableIds(true)
+            setHasStableIds(this@FileListFragment !is SearchFragment)
 
             onEmptyList = { checkIfNoFiles() }
 
