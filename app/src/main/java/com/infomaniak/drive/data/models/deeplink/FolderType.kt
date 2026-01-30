17c7df751e8f34e8e5e63d696f727db763943730
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-01-30 07:49:47 +0100

    fix: Change folder type to reflect the other source of truth

--- app/src/main/java/com/infomaniak/drive/data/models/deeplink/FolderType.kt
+++ app/src/main/java/com/infomaniak/drive/data/models/deeplink/FolderType.kt
@@ -17,66 +17,37 @@
  */
 package com.infomaniak.drive.data.models.deeplink
 
+import com.infomaniak.drive.data.models.deeplink.ExternalFileType.Companion.extractExternalFileType
+import com.infomaniak.drive.data.models.deeplink.FileType.Companion.extractFileType
 
-enum class FolderType(val type: String, val builder: (FileType) -> RoleFolder) {
-    Collaboratives(type = "collaboratives", builder = RoleFolder::Collaboratives),
-    Favorites(type = "favorites", builder = RoleFolder::Favorites),
-    File(type = "file", builder = RoleFolder::File),
-    MyShares(type = "my-shares", builder = RoleFolder::MyShare),
-    SharedWithMe(type = "shared-with-me", builder = RoleFolder::SharedWithMe),
-    SharedLinks(type = "shared-links", builder = RoleFolder::SharedLinks),
-    Recent(type = "recent", builder = RoleFolder::Recent),
-    Trash(type = "trash", builder = RoleFolder::Trash);
+
+enum class FolderType(val type: String, val propertiesPattern: String) {
+    Collaboratives(type = "collaboratives", propertiesPattern = FileType.FOLDER_PROPERTIES),
+    Favorites(type = "favorites", propertiesPattern = PREVIEW),
+    File(type = "file", propertiesPattern = ""),
+    MyShares(type = "my-shares", propertiesPattern = PREVIEW),
+    SharedWithMe(type = "shared-with-me", propertiesPattern = ExternalFileType.SHARED_WITH_ME_FOLDER_PROPERTIES),
+    SharedLinks(type = "shared-links", propertiesPattern = PREVIEW),
+    Recent(type = "recent", propertiesPattern = PREVIEW),
+    Trash(type = "trash", propertiesPattern = FOLDER_ID);
+
+    fun build(folderProperties: String): RoleFolder = folderProperties.optionalFind(propertiesPattern).run {
+        when (this@FolderType) {
+            Collaboratives -> RoleFolder.Collaboratives()
+            Favorites -> RoleFolder.Favorites(fileId = parseOptionalId(3))
+            File -> RoleFolder.File(fileType = extractFileType())
+            MyShares -> RoleFolder.MyShare(fileId = parseOptionalId(3))
+            SharedWithMe -> RoleFolder.SharedWithMe(fileType = extractExternalFileType())
+            SharedLinks -> RoleFolder.SharedLinks(fileId = parseOptionalId(3))
+            Recent -> RoleFolder.Recent(fileId = parseOptionalId(3))
+            Trash -> RoleFolder.Trash(folderId = parseOptionalId(1))
+        }
+    }
 
     companion object {
-        const val GROUP_NAME_FILE = "file"
-        const val FILE = "$FOLDER_ID/$FILE_ID"
-
-        const val GROUP_NAME_PREVIEW = "preview"
-        const val PREVIEW = "$KEY_PREVIEW/$FILE_TYPE/$FILE_ID"
-        const val GROUP_NAME_PREVIEW_FOLDER = "previewFolder"
-        const val PREVIEW_FOLDER = "$FOLDER_ID/$KEY_PREVIEW/$FILE_TYPE/$FILE_ID"
-        const val SOURCE_DRIVE_FILE = "$DRIVE_ID/$FOLDER_ID/$FILE_ID"
-        const val SOURCE_DRIVE_PREVIEW_FOLDER = "$DRIVE_ID/$FOLDER_ID/$KEY_PREVIEW/$FILE_TYPE/$FILE_ID"
-
-        /**
-         * GENERIC_FOLDER_PROPERTIES filters this kind of paths :
-         *      <folderId>/<fileId>
-         *      preview/<type:string>/<fileId>
-         *      <folderId>/preview/<type:string>/<fileId>
-         *  It find one of them and assign it to a named group
-         */
-        const val GENERIC_FOLDER_PROPERTIES =
-            "(?<$GROUP_NAME_FILE>$FILE)|(?<$GROUP_NAME_PREVIEW>$PREVIEW)|(?<$GROUP_NAME_PREVIEW_FOLDER>$PREVIEW_FOLDER)"
-
-        /**
-         * SHARED_WITH_ME_FOLDER_PROPERTIES filters this kind of paths :
-         *       <sourceDriveId>/<folderId>/<fileId>
-         *       <sourceDriveId>/<folderId>/preview/<type:string>/<fileId>
-         *  It find one of them and assign it to a named group
-         */
-        const val SHARED_WITH_ME_FOLDER_PROPERTIES =
-            "(?<$GROUP_NAME_FILE>$SOURCE_DRIVE_FILE)|(?<$GROUP_NAME_PREVIEW_FOLDER>$SOURCE_DRIVE_PREVIEW_FOLDER)"
         @Throws(InvalidValue::class)
         fun from(value: String): FolderType = entries.find { it.type == value } ?: throw InvalidValue()
 
-        @Throws(InvalidValue::class)
-        private fun MatchResult?.convertToDoubleDriveFileType(): FileType = this?.let {
-            groups[GROUP_NAME_FILE]?.let { FileType.ExternalFile(this) }
-                ?: groups[GROUP_NAME_PREVIEW_FOLDER]?.let { FileType.ExternalFilePreviewInFolder(this) }
-        } ?: throw InvalidValue()
-
-        @Throws(InvalidValue::class)
-        private fun MatchResult?.convertToFileType(): FileType = this?.let {
-            groups[GROUP_NAME_FILE]?.let { FileType.File(this) }
-                ?: groups[GROUP_NAME_PREVIEW]?.let { FileType.FilePreview(this) }
-                ?: groups[GROUP_NAME_PREVIEW_FOLDER]?.let { FileType.FilePreviewInFolder(this) }
-        } ?: throw InvalidValue()
-
-        fun FolderType.build(folderProperties: String): RoleFolder = when (this) {
-            SharedWithMe -> Regex(SHARED_WITH_ME_FOLDER_PROPERTIES).find(folderProperties).convertToDoubleDriveFileType()
-            else -> Regex(GENERIC_FOLDER_PROPERTIES).find(folderProperties).convertToFileType()
-        }.let(builder)
-
+        fun String.optionalFind(propertiesPattern: String): MatchResult? = Regex(propertiesPattern).find(this)
     }
 }
