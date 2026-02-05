87845e80030824fb9b5860a3ce3cef0fc955808c
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-02-05 07:24:47 +0100

    fix: Throw NotImplemented only if in Debug build, for developer purpose only

--- app/src/main/java/com/infomaniak/drive/ui/MainActivity.kt
+++ app/src/main/java/com/infomaniak/drive/ui/MainActivity.kt
@@ -74,6 +74,7 @@ import com.infomaniak.core.legacy.utils.UtilsUi.generateInitialsAvatarDrawable
 import com.infomaniak.core.legacy.utils.UtilsUi.getBackgroundColorBasedOnId
 import com.infomaniak.core.legacy.utils.setMargins
 import com.infomaniak.core.legacy.utils.whenResultIsOk
+import com.infomaniak.drive.BuildConfig.DEBUG
 import com.infomaniak.drive.GeniusScanUtils.scanResultProcessing
 import com.infomaniak.drive.GeniusScanUtils.startScanFlow
 import com.infomaniak.drive.MatomoDrive.MatomoCategory
@@ -92,7 +93,6 @@ import com.infomaniak.drive.data.models.File.VisibilityType
 import com.infomaniak.drive.data.models.UiSettings
 import com.infomaniak.drive.data.models.UploadFile
 import com.infomaniak.drive.data.models.UserDrive
-import com.infomaniak.drive.data.models.file.SpecialFolder
 import com.infomaniak.drive.data.models.deeplink.DeeplinkAction
 import com.infomaniak.drive.data.models.deeplink.DeeplinkType
 import com.infomaniak.drive.data.models.drive.Drive
@@ -281,7 +281,7 @@ class MainActivity : BaseActivity() {
     }
 
     private fun handleCollaborateDeeplink(link: DeeplinkAction.Collaborate) {
-        if (link.isHandled) TODO("Need to implement here when Collaborate deeplink will be supported")
+        if (DEBUG && link.isHandled) TODO("Need to implement here when Collaborate deeplink will be supported")
     }
 
     private fun handleDriveDeeplink(link: DeeplinkAction.Drive) {
