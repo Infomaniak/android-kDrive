b3dfdab94b154252939c2b41c8c6817e49903bfe
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-02-10 11:06:17 +0100

    chore: Simplify notification building

--- app/src/main/java/com/infomaniak/drive/data/api/UploadTask.kt
+++ app/src/main/java/com/infomaniak/drive/data/api/UploadTask.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak kDrive - Android
- * Copyright (C) 2022-2025 Infomaniak Network SA
+ * Copyright (C) 2022-2026 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -433,7 +433,7 @@ class UploadTask(
 
         if (uploadNotificationElapsedTime >= ELAPSED_TIME) {
             uploadNotification.apply {
-                setContentIntent(uploadFile.progressPendingIntent(context))
+                setContentIntent(uploadFile.progressPendingIntent())
                 setContentText("${progress}%")
                 setProgress(100, progress, false)
                 notificationManagerCompat.notifyCompat(CURRENT_UPLOAD_ID, this)
--- app/src/main/java/com/infomaniak/drive/data/services/UploadWorker.kt
+++ app/src/main/java/com/infomaniak/drive/data/services/UploadWorker.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak kDrive - Android
- * Copyright (C) 2022-2025 Infomaniak Network SA
+ * Copyright (C) 2022-2026 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -41,6 +41,7 @@ import com.infomaniak.core.legacy.utils.getFileName
 import com.infomaniak.core.legacy.utils.getFileSize
 import com.infomaniak.core.legacy.utils.hasPermissions
 import com.infomaniak.core.network.api.ApiController.gson
+import com.infomaniak.core.notifications.notifyCompat
 import com.infomaniak.core.sentry.SentryLog
 import com.infomaniak.drive.R
 import com.infomaniak.drive.data.api.FileChunkSizeManager.AllowedFileSizeExceededException
@@ -61,7 +62,6 @@ import com.infomaniak.drive.utils.MediaFoldersProvider.VIDEO_BUCKET_ID
 import com.infomaniak.drive.utils.NotificationUtils
 import com.infomaniak.drive.utils.NotificationUtils.buildGeneralNotification
 import com.infomaniak.drive.utils.NotificationUtils.cancelNotification
-import com.infomaniak.drive.utils.NotificationUtils.notifyCompat
 import com.infomaniak.drive.utils.SyncUtils
 import com.infomaniak.drive.utils.getAvailableMemory
 import com.infomaniak.drive.utils.uri
@@ -361,7 +361,7 @@ class UploadWorker(appContext: Context, params: WorkerParameters) : CoroutineWor
     }
 
     private fun progressForegroundInfo(pendingCount: Int): ForegroundInfo {
-        val notification = UploadNotifications.getCurrentUploadNotification(applicationContext, pendingCount).build()
+        val notification = UploadNotifications.getCurrentUploadNotification(pendingCount).build()
         val foregroundInfo = when {
             SDK_INT >= 29 -> {
                 ForegroundInfo(
