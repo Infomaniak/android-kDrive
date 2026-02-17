3076ae7db2d9f970816e24fc9cafcd9bd7477f74
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-02-17 14:25:04 +0100

    chore: Make action menu hidden by default

--- app/src/main/java/com/infomaniak/drive/ui/fileList/FileAdapter.kt
+++ app/src/main/java/com/infomaniak/drive/ui/fileList/FileAdapter.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak kDrive - Android
- * Copyright (C) 2022-2024 Infomaniak Network SA
+ * Copyright (C) 2022-2026 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -368,15 +368,21 @@ open class FileAdapter(
         filePreview.isInvisible = file.isImporting() || (isSelectedFile(file) && !isGrid)
     }
 
-    private fun FileItemViewHolder.setupMenuButton(file: File) = menuButton.apply {
-        isGone = uploadInProgress
+    private fun FileItemViewHolder.setupMenuButton(file: File) {
+        onMenuClicked?.takeUnless { hideMenu(file) }
+            ?.let { onMenuClicked ->
+                menuButton.isVisible = true
+                menuButton.setOnClickListener { onMenuClicked(file) }
+            }
+    }
+
+    private fun hideMenu(file: File): Boolean {
+        return uploadInProgress
                 || isSelectingFolder
                 || file.isFromActivities
                 || file.isFromSearch
                 || (offlineMode && !file.isOffline)
                 || !publicShareCanDownload
-
-        setOnClickListener { onMenuClicked?.invoke(file) }
     }
 
     private fun FileItemViewHolder.setupCardClicksListeners(file: File, position: Int) = with(multiSelectManager) {
