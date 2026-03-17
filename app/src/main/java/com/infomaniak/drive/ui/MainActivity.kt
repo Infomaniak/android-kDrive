5cd03dff26e87b68f61cc1fc291e81946fd3f490
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-03-17 14:14:31 +0100

    feat: Add  host whitelist for WebViewActivity to prevent open malicious url
    # Conflicts:
    #	Core
    
    # Conflicts:
    #	Core

--- app/src/main/java/com/infomaniak/drive/ui/MainActivity.kt
+++ app/src/main/java/com/infomaniak/drive/ui/MainActivity.kt
@@ -321,6 +321,7 @@ class MainActivity : BaseActivity() {
             context = this@MainActivity,
             url = url,
             headers = AccountUtils.currentUser?.run { mapOf("Authorization" to "Bearer ${apiToken.accessToken}") },
+            hostWhiteList = arrayListOf("ksuite.infomaniak.com", "kdrive.infomaniak.com"),
         )
     }
 
