e698fffff95edf68a9f491bc109b5969ae6e0c33
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-03-23 14:15:08 +0100

    refactor: Order languages alphabetically like in other places

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
         resValue("string", "EXPOSED_PUBLIC_SHARE_DIR", "public_share")
 
         androidResources {
-            localeFilters += listOf("en", "de", "es", "fr", "it", "da", "el", "fi", "nb", "nl", "pl", "pt", "sv")
+            localeFilters += listOf("da", "de", "el", "en", "es", "fi", "fr", "it", "nb", "nl", "pl", "pt", "sv")
             generateLocaleConfig = true
         }
     }
