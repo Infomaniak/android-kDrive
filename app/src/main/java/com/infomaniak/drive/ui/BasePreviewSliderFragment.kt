845ba743354dc018a44e35e5d118ad15af61ec6f
Aymeric MARIAUX <aymeric.mariaux@infomaniak.com>
2026-06-19 14:04:09 +0200

    fix: Align offline preview availability with each preview's local source

--- app/src/main/java/com/infomaniak/drive/ui/BasePreviewSliderFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/BasePreviewSliderFragment.kt
@@ -170,7 +170,7 @@ abstract class BasePreviewSliderFragment : Fragment(), FileInfoActionsView.OnIte
 
         viewLifecycleOwner.lifecycleScope.launch {
             mainViewModel.isNetworkAvailable.collectLatest { isNetworkAvailable ->
-                (bottomSheetView as? FileInfoActionsView)?.updateNetworkAvailability(isNetworkAvailable != false)
+                (bottomSheetView as? FileInfoActionsView)?.updateNetworkAvailability(isNetworkAvailable)
             }
         }
     }
