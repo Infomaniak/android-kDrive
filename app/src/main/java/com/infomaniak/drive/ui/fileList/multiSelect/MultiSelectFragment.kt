a575d289ccafcb3ae279dfba12b549a4f4a82f78
Aymeric MARIAUX <aymeric.mariaux@infomaniak.com>
2026-07-17 14:02:46 +0200

    fix: Keep deselected folders as valid destinations in a "select all" move

--- app/src/main/java/com/infomaniak/drive/ui/fileList/multiSelect/MultiSelectFragment.kt
+++ app/src/main/java/com/infomaniak/drive/ui/fileList/multiSelect/MultiSelectFragment.kt
@@ -231,6 +231,7 @@ abstract class MultiSelectFragment(private val matomoCategory: MatomoCategory) :
             mainViewModel = mainViewModel,
             filesToMove = getValidSelectedItems(),
             disabledNavigationParentFolderId = if (isSelectAllOn) currentFolder?.id else null,
+            exceptedNavigationFolderIds = if (isSelectAllOn) exceptedItemsIds.toIntArray() else null,
         )
     }
 
