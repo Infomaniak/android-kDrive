8fae9a460e7428284d186168f92ee5b055a7188a
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-08-24 13:19:14 +0200

    chore: Bump agp9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,6 @@ import java.util.Properties
 plugins {
     alias(core.plugins.android.application)
     alias(libs.plugins.junit5)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.navigation.safeargs)
     alias(libs.plugins.realm.android)
     alias(core.plugins.ksp)
@@ -30,7 +28,7 @@ android {
 
     compileSdk = appCompileSdk
 
-    ndkVersion = "28.0.13004108"
+    ndkVersion = "30.0.14904198"
 
     defaultConfig {
         testInstrumentationRunnerArguments["runnerBuilder"] = "de.mannodermaus.junit5.AndroidJUnit5Builder"
@@ -39,8 +37,6 @@ android {
         targetSdk = appTargetSdk
         versionCode = 5_021_001_01
         versionName = "5.21.1"
-
-        setProperty("archivesBaseName", "kdrive-$versionName ($versionCode)")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "CLIENT_ID", "\"9473D73C-C20F-4971-9E10-D957C563FA68\"")
@@ -97,6 +93,7 @@ android {
         viewBinding = true
         buildConfig = true
         compose = true
+        resValues = true
     }
 
     packaging {
@@ -112,6 +109,10 @@ android {
     testOptions.unitTests.all {
         it.testLogging { events("passed", "skipped", "failed", "standardOut", "standardError") }
     }
+}
+
+base {
+    archivesName = "kdrive-${android.defaultConfig.versionName} (${android.defaultConfig.versionCode})"
 }
 
 kotlin {
